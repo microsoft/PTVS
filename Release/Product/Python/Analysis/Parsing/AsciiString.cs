2e2c0855a584c8f3de2068b6678e5a2916acfa29
Dino Viehland <dinov@microsoft.com>
2011-03-31 13:34:04 -0700

    Add missing files

---
+++ Release/Product/Python/Analysis/Parsing/AsciiString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.PythonTools.Parsing {
+    public sealed class AsciiString {
+        private readonly byte[] _bytes;
+        private string _str;
+
+        public AsciiString(byte[] bytes, string str) {
+            _bytes = bytes;
+            _str = str;
+        }
+
+        public byte[] Bytes {
+            get {
+                return _bytes;
+            }
+        }
+
+        public string String {
+            get {
+                return _str;
+            }
+        }
+    }
+}
