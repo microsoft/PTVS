89b3168ed4ed6d48ad3a8f057ce794ffb7710aef
Hugues Valois <huvalo@microsoft.com>
2013-01-29 15:44:17 -0800

    Removed obsolete rolling run code.

--- Tools/RollingRun/RollingRunHost/RollingRunHost/buildptvs.bat
+++
@@ -1,9 +0,0 @@
-cd %~dp0
-pushd ..\..\..\..\release\product\setup
-powershell .\buildrelease.ps1 \\wtestserver\Build\ptvs
-robocopy /s /e ..\..\..\..\binaries\release\completionDB\* \\wtestserver\build\ptvs\release\completiondb\
-robocopy /s /e ..\..\..\..\binaries\release\Python.VS.TestData\* \\wtestserver\build\ptvs\release\Python.VS.TestData\
-copy ..\..\..\..\binaries\release\*.py \\wtestserver\build\ptvs\release\
-
-popd
-
