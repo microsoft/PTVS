79d78d65a1f3d1f6328800e8703cc50f85197537
Steve Dower <steve.dower@microsoft.com>
2014-01-16 15:50:08 -0800

    Simplifies the Flask template to only include app.py. Previously we used a separate script to launch the app with externally-specified port and host settings, but this was more complicated than necessary. We now provide an app.py that will inspect the environment variables (set by the debug launcher) and remove runserver.py completely.

--- Python/Product/PythonTools/Templates/Projects/WebProjectFlask/app.py
+++ Python/Product/PythonTools/Templates/Projects/WebProjectFlask/app.py
@@ -10,4 +10,10 @@ def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run()
+    import os
+    host = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        port = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        port = 5555
+    app.run(host, port)
