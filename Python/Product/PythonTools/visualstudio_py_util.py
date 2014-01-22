f7cd7f56f5ccc87352a3c822fe7696372cbe1f5f
Pavel Minaev <pminaev@microsoft.com>
2014-01-22 12:06:51 -0800

    #2072 Debugging is slow, laggy and finally collapses
    Special-cases deque and OrderedDict (when available) to use our custom repr logic.
    
    Adds logic to try to handle overly long iterables of non-special-cased types by avoiding calling repr() on them if they are too long, or contain nested iterables that are too long, or are nested too deep (using same limits as for special-cased collection types). Exclude iterator-like objects from this from these rules, as well as range/xrange, since those are known to have O(1) reprs.

--- Python/Product/PythonTools/visualstudio_py_util.py
+++ Python/Product/PythonTools/visualstudio_py_util.py
@@ -23,11 +23,16 @@ from encodings import utf_8, ascii
 # WARNING: Avoid imports beyond this point, specifically on the debugger thread, as this may cause
 # deadlock where the debugger thread performs an import while a user thread has the import lock
 
-# Py3k compat - alias unicode to str
+# Py3k compat - alias unicode to str, and xrange to range
 try:
     unicode
 except:
     unicode = str
+try:
+    xrange
+except:
+    xrange = range
+
 
 if sys.version_info[0] >= 3:
     def to_bytes(cmd_str):
@@ -175,25 +180,37 @@ class SafeRepr(object):
         set_info = (set, '{', '}', False)
         frozenset_info = (frozenset, 'frozenset({', '})', False)
     else:
-        string_types = (basestring,)
+        string_types = (str, unicode)
         set_info = (set, 'set([', '])', False)
         frozenset_info = (frozenset, 'frozenset([', '])', False)
-    
+
     # Collection types are recursively iterated for each limit in
     # maxcollection.
     maxcollection = (15, 10)
+
     # Specifies type, prefix string, suffix string, and whether to include a
     # comma if there is only one element. (Using a sequence rather than a
     # mapping because we use isinstance() to determine the matching type.)
-    collection_types = (
+    collection_types = [
         (tuple, '(', ')', True),
         (list, '[', ']', False),
         frozenset_info,
         set_info,
-    )
-    
-    dict_types = (dict,)
-    
+    ]
+    try:
+        from collections import deque
+        collection_types.append((deque, 'deque([', '])', False))
+    except:
+        pass
+
+    # type, prefix string, suffix string, item prefix string, item key/value separator, item suffix string
+    dict_types = [(dict, '{', '}', '', ': ', '')]
+    try:
+        from collections import OrderedDict
+        dict_types.append((OrderedDict, 'OrderedDict([', '])', '(', ', ', ')'))
+    except:
+        pass
+
     # All other types are treated identically to strings, but using
     # different limits.
     maxother_outer = 2 ** 16
@@ -207,23 +224,76 @@ class SafeRepr(object):
                 return 'An exception was raised: %r' % sys.exc_info()[1]
             except:
                 return 'An exception was raised'
-    
+
     def _repr(self, obj, level):
         '''Returns an iterable of the parts in the final repr string.'''
+
         obj_repr = type(obj).__repr__
+        def has_obj_repr(t):
+            try:
+                r = t.__repr__
+            except:
+                return False
+            try:
+                return obj_repr == r
+            except:
+                return obj_repr is r
+
         for t, prefix, suffix, comma in self.collection_types:
-            if isinstance(obj, t) and t.__repr__ is obj_repr:
+            if isinstance(obj, t) and has_obj_repr(t):
                 return self._repr_iter(obj, level, prefix, suffix, comma)
         
-        for t in self.dict_types:
-            if isinstance(obj, t) and t.__repr__ is obj_repr:
-                return self._repr_dict(obj, level)
+        for t, prefix, suffix, item_prefix, item_sep, item_suffix in self.dict_types:
+            if isinstance(obj, t) and has_obj_repr(t):
+                return self._repr_dict(obj, level, prefix, suffix, item_prefix, item_sep, item_suffix)
 
         for t in self.string_types:
-            if isinstance(obj, t) and t.__repr__ is obj_repr:
+            if isinstance(obj, t) and has_obj_repr(t):
                 return self._repr_str(obj, level)
+
+        if self._is_long_iter(obj):
+            return self._repr_long_iter(obj)
         
         return self._repr_other(obj, level)
+
+    # Determines whether an iterable exceeds the limits set in maxlimits, and is therefore unsafe to repr().
+    def _is_long_iter(self, obj, level = 0):
+        if not hasattr(obj, '__iter__') and not hasattr(obj, '__getitem__'):
+            return False
+
+        try:
+            # Iterable is its own iterator - this is a one-off iterable like generator or enumerate(). We can't
+            # really count that, but repr() for these should not include any elements anyway, so we can treat it
+            # the same as non-iterables.
+            if obj is iter(obj):
+                return False
+
+            # xrange reprs fine regardless of length.
+            if isinstance(obj, xrange):
+                return False
+
+            # Strings have their own limits (and do not nest).
+            if isinstance(obj, self.string_types):
+                return len(obj) > self.maxstring_inner
+
+            # Iterables that nest too deep are considered long.
+            if level >= len(self.maxcollection):
+                return True
+
+            # It is too long if the length exceeds the limit, or any of its elements are long iterables.
+            if hasattr(obj, '__len__'):
+                try:
+                    l = len(obj)
+                except:
+                    l = None
+                if l is not None and l > self.maxcollection[level]:
+                    return True
+                return any((self._is_long_iter(item, level + 1) for item in obj))
+            return any(i > self.maxcollection[level] or self._is_long_iter(item, level + 1) for i, item in enumerate(obj))
+
+        except:
+            # If anything breaks, assume the worst case.
+            return True
     
     def _repr_iter(self, obj, level, prefix, suffix, comma_after_single_element = False):
         yield prefix
@@ -249,16 +319,26 @@ class SafeRepr(object):
                 if comma_after_single_element and count == self.maxcollection[level] - 1:
                     yield ','
         yield suffix
+
+    def _repr_long_iter(self, obj):
+        try:
+            obj_repr = '<%s, len() = %s>' % (type(obj).__name__, len(obj))
+        except:
+            try:
+                obj_repr = '<' + type(obj).__name__ + '>'
+            except:
+                obj_repr = '<no repr available for object>'
+        yield obj_repr
         
-    def _repr_dict(self, obj, level):
+    def _repr_dict(self, obj, level, prefix, suffix, item_prefix, item_sep, item_suffix):
         if not obj:
-            yield '{}'
+            yield prefix + suffix
             return
         if level >= len(self.maxcollection):
-            yield '{...}'
+            yield prefix + '...' + suffix
             return
         
-        yield '{'
+        yield prefix
         
         count = self.maxcollection[level]
         yield_comma = False
@@ -278,9 +358,12 @@ class SafeRepr(object):
                 yield '...'
                 break
             
+            yield item_prefix
             for p in self._repr(key, level + 1):
                 yield p
-            yield ': '
+
+            yield item_sep
+
             try:
                 item = obj[key]
             except Exception:
@@ -288,12 +371,13 @@ class SafeRepr(object):
             else:
                 for p in self._repr(item, 100 if item is obj else level + 1):
                     yield p
+            yield item_suffix
         
-        yield '}'
-    
+        yield suffix
+
     def _repr_str(self, obj, level):
         return self._repr_obj(obj, level, self.maxstring_inner, self.maxstring_outer)
-    
+
     def _repr_other(self, obj, level):
         return self._repr_obj(obj, level, self.maxother_inner, self.maxother_outer)
     
@@ -443,19 +527,37 @@ class SafeRepr(object):
                 return self.repr_str
         re_test(TestClass(), r'\<A+\.\.\.A+\>')
 
-        # Test collections that override repr
-        class TestClass(dict):
-            def __repr__(self): return 'MyRepr'
-        test(TestClass(), 'MyRepr')
-        class TestClass(list):
-            def __repr__(self): return 'MyRepr'
-        test(TestClass(), 'MyRepr')
-
         # Test collections that don't override repr
         class TestClass(dict): pass
         test(TestClass(), '{}')
         class TestClass(list): pass
         test(TestClass(), '[]')
+
+        # Test collections that override repr
+        class TestClass(dict):
+            def __repr__(self): return 'MyRepr'
+        test(TestClass(), 'MyRepr')
+        class TestClass(list):
+            def __init__(self, iter = ()): list.__init__(self, iter)
+            def __repr__(self): return 'MyRepr'
+        test(TestClass(), 'MyRepr')
+
+        # Test collections and iterables with long repr
+        test(TestClass(xrange(0, 15)), 'MyRepr')
+        test(TestClass(xrange(0, 16)), '<TestClass, len() = 16>')
+        test(TestClass([TestClass(xrange(0, 10))]), 'MyRepr')
+        test(TestClass([TestClass(xrange(0, 11))]), '<TestClass, len() = 1>')
+
+        # Test strings inside long iterables
+        test(TestClass(['a' * (self.maxcollection[1] + 1)]), 'MyRepr')
+        test(TestClass(['a' * (self.maxstring_inner + 1)]), '<TestClass, len() = 1>')
+
+        # Test range
+        if sys.version[0] == '2':
+            range_name = 'xrange'
+        else:
+            range_name = 'range'
+        test(xrange(1, self.maxcollection[0] + 1), '%s(1, %s)' % (range_name, self.maxcollection[0] + 1))
 
         # Test directly recursive collections
         c1 = [1, 2]
@@ -483,4 +585,5 @@ class SafeRepr(object):
         assert len(text) < 8192
 
 if __name__ == '__main__':
+    print('Running tests...')
     SafeRepr()._selftest()
