e258e8df8fbb4189b4c9667569a9d3f507cf7dab
Steve Dower <steve.dower@microsoft.com>
2014-01-24 13:41:25 -0800

    Makes the default Bottle app more interesting. Changes to using bottle.default_app() as the WSGI application. Adds explanatory comments to the template.

--- Python/Product/PythonTools/Templates/Projects/WebProjectBottle/app.py
+++ Python/Product/PythonTools/Templates/Projects/WebProjectBottle/app.py
@@ -1,25 +1,34 @@
 import bottle
 import os
 import sys
+from bottle import route, template, redirect
 
 if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
+    # Debug mode will enable more verbose output in the console window.
+    # It must be set at the beginning of the script.
     bottle.debug(True)
 
 
-app = bottle.Bottle()
-
-@app.route('/')
+@route('/')
 def hello():
-    return "Hello World!"
+    redirect('/hello/world')
+
+@route('/hello/<name>')
+def example(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
 
 
-# Make the WSGI interface available at the top level so wfastcgi can get it.
-wsgi_app = app
+
+def wsgi_app():
+    # Returns the application to make available through wfastcgi. This is used
+    # when the site is published to Windows Azure.
+    return bottle.default_app()
 
 if __name__ == '__main__':
+    # Starts a local test server.
     host = os.environ.get('SERVER_HOST', 'localhost')
     try:
         port = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         port = 5555
-    app.run(server='wsgiref', host=host, port=port)
+    bottle.run(server='wsgiref', host=host, port=port)
