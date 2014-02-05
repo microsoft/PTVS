8345c49674bebbc1db8ce366a4df8e3aa44bcc28
Steve Dower <steve.dower@microsoft.com>
2014-02-05 09:58:23 -0800

    Adds test for modules['__main__'].__dict__ is globals().

--- Python/Tests/TestData/DebuggerProject/CheckNameAndFile.py
+++ Python/Tests/TestData/DebuggerProject/CheckNameAndFile.py
@@ -1,4 +1,5 @@
-from sys import argv, exit
+from sys import argv, exit, modules
 print(argv)
 if __name__ != '__main__': exit(1)
 if __file__ != argv[0]: exit(2)
+if modules['__main__'].__dict__ is not globals(): exit(3)
