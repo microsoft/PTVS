2df1c9f6b9833c1ae44a62d3dfbe208e2e42805a
Pavel Minaev <pminaev@microsoft.com>
2014-06-24 12:16:11 -0700

    Update ptvsd version number for 2.0 beta 2.

--- Python/Product/PythonTools/ptvsd/setup.py
+++ Python/Product/PythonTools/ptvsd/setup.py
@@ -18,7 +18,7 @@
 from distutils.core import setup
 
 setup(name='ptvsd',
-      version='2.1.0b1',
+      version='2.1.0b2',
       description='Python Tools for Visual Studio remote debugging server',
       license='Apache License 2.0',
       author='Microsoft Corporation',
