64ccdc2c7a4ed21595fae0542f0876561f85834e
Steve Dower <steve.dower@microsoft.com>
2014-08-12 16:53:41 -0700

    #2586 bottle template has unnessecary import Adds comment explaining why import routes is necessary. Adds no-op reference to routes module so that Remove Imports does not remove it.

--- Python/Product/PythonTools/Templates/Projects/StarterBottleProject/app.py
+++ Python/Product/PythonTools/Templates/Projects/StarterBottleProject/app.py
@@ -5,6 +5,8 @@ This script runs the application using a development server.
 import bottle
 import os
 import sys
+
+# routes contains the HTTP handlers for our server and must be imported.
 import routes
 
 if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
