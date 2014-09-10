2bfcb04ee9f264de834715a707282c6e28033dd4
Steve Dower <steve.dower@microsoft.com>
2014-09-10 10:46:57 -0700

    #2666 Error publishing cloud service on Express for Web Adds fallback for settings sources to look in Web Express and Desktop Express if Pro+ is not installed.

--- Python/Product/VSInterpreters/SettingsManagerCreator.cs
+++ Python/Product/VSInterpreters/SettingsManagerCreator.cs
@@ -47,12 +47,30 @@ namespace Microsoft.VisualStudioTools {
 
             if (settings == null) {
                 if (!File.Exists(devenvPath)) {
+                    // Running outside VS, so we need to guess which SKU of VS
+                    // is being used. This will work correctly if any one SKU is
+                    // installed, but may select the wrong SKU if multiple are
+                    // installed. As a result, custom environments may not be
+                    // available when building or testing Python projects.
+                    string devenvRoot = null;
                     using (var root = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32))
                     using (var key = root.OpenSubKey(string.Format(@"Software\Microsoft\VisualStudio\{0}\Setup\VS", AssemblyVersionInfo.VSVersion))) {
-                        if (key == null) {
-                            throw new InvalidOperationException("Cannot find settings store for Visual Studio " + AssemblyVersionInfo.VSVersion);
+                        if (key != null) {
+                            devenvRoot = key.GetValue("ProductDir") as string;
                         }
-                        devenvPath = key.GetValue("EnvironmentPath") as string;
+                    }
+                    if (Directory.Exists(devenvRoot)) {
+                        foreach(var subPath in new[] {
+                            "Common7\\IDE\\devenv.exe",
+                            "Common7\\IDE\\vwdexpress.exe",
+                            "Common7\\IDE\\wdexpress.exe"
+                        }) {
+                            devenvPath = Path.Combine(devenvRoot, subPath);
+                            if (File.Exists(devenvPath)) {
+                                break;
+                            }
+                            devenvPath = null;
+                        }
                     }
                 }
                 if (!File.Exists(devenvPath)) {
