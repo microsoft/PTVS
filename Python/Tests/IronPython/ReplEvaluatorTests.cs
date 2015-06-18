f90640cedeea17abf0c83771892c1310f4161244
Steve Dower <steve.dower@microsoft.com>
2015-06-18 11:00:15 -0700

    Adds retry in GenericMethodCompletions for when GetMemberNames times out.

--- Python/Tests/IronPython/ReplEvaluatorTests.cs
+++ Python/Tests/IronPython/ReplEvaluatorTests.cs
@@ -16,6 +16,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Microsoft.IronPythonTools.Interpreter;
+using Microsoft.PythonTools.Analysis;
 using Microsoft.PythonTools.Intellisense;
 using Microsoft.PythonTools.Interpreter;
 using Microsoft.PythonTools.Parsing;
@@ -145,7 +146,11 @@ namespace ReplWindowUITests {
             using (var analyzer = new VsProjectAnalyzer(PythonToolsTestUtilities.CreateMockServiceProvider(), fact, new[] { fact })) {
                 replWindow.TextView.TextBuffer.Properties.AddProperty(typeof(VsProjectAnalyzer), analyzer);
 
-                var names = replEval.GetMemberNames("t");
+                MemberResult[] names = null;
+                for (int retries = 0; retries < 5 && names == null; retries += 1) {
+                    names = replEval.GetMemberNames("t");
+                }
+                Assert.IsNotNull(names, "GetMemberNames call timed out");
                 foreach (var name in names) {
                     Debug.WriteLine(name.Name);
                 }
