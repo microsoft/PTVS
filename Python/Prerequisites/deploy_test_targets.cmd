6aba727e81706a35a4185a830b12293148d3edc5
Steve Dower <steve.dower@microsoft.com>
2015-07-15 08:37:09 -0700

    Adds script to deploy test targets.

---
+++ Python/Prerequisites/deploy_test_targets.cmd
@@ -0,0 +1,37 @@
+@setlocal
+@echo off
+rem
+rem The test targets files redirect projects to use the targets deployed
+rem to the Experimental hive. By installing these targets, you can test
+rem modifications easily without having to manually redeploy files to
+rem your MSBuild directory.
+rem
+rem This script should be run as an administrator.
+rem
+
+echo This script should be run as an administrator.
+
+set D=%~dp0
+
+call :docopy "%ProgramFiles(x86)%\MSBuild\Microsoft\VisualStudio\v14.0"
+call :docopy "%ProgramFiles(x86)%\MSBuild\Microsoft\VisualStudio\v12.0"
+
+pause
+exit /B 0
+
+:docopy
+
+set TARGET=%~1\Python Tools\
+
+if not exist "%~1" exit /B 0
+
+pushd "%D%..\Product\BuildTasks\TestTargets"
+echo.
+echo Copying:
+echo     from %CD%
+echo     to %TARGET%
+if not exist "%TARGET%" mkdir "%TARGET%"
+copy /Y "*.targets" "%TARGET%"
+popd
+
+exit /B 0
