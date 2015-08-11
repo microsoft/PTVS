c0d2d63b8dd7ef9d599e87509a5d5b8fd12f4509
Steve Dower <steve.dower@microsoft.com>
2015-08-11 11:58:43 -0700

    Fixes #712: Don't add Unicode BOM by default Changes default encoding to be UTF-8 without a signature.

--- Python/Product/Analysis/Parsing/Parser.cs
+++ Python/Product/Analysis/Parsing/Parser.cs
@@ -4831,7 +4831,7 @@ namespace Microsoft.PythonTools.Parsing {
         /// New in 1.1.
         /// </summary>
         public static Encoding GetEncodingFromStream(Stream stream) {
-            return GetStreamReaderWithEncoding(stream, Encoding.UTF8, ErrorSink.Null).CurrentEncoding;
+            return GetStreamReaderWithEncoding(stream, new UTF8Encoding(false), ErrorSink.Null).CurrentEncoding;
         }
 
         private static StreamReader/*!*/ GetStreamReaderWithEncoding(Stream/*!*/ stream, Encoding/*!*/ defaultEncoding, ErrorSink errors) {
