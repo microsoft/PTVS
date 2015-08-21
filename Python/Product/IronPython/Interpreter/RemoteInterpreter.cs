a8b02a4478b5365329373b7b01aac1c7d23c4fcc
Steve Dower <steve.dower@microsoft.com>
2015-08-21 15:50:28 -0700

    Fixes #697 Exception while editing IronPython code Ensures generic types are wrapped and unwrapped correctly.

--- Python/Product/IronPython/Interpreter/RemoteInterpreter.cs
+++ Python/Product/IronPython/Interpreter/RemoteInterpreter.cs
@@ -1055,9 +1055,13 @@ namespace Microsoft.IronPythonTools.Interpreter {
                 if (genType != null) {
                     Type[] genTypes = new Type[types.Length];
                     for (int i = 0; i < types.Length; i++) {
-                        genTypes[i] = (Type)Unwrap(types[i]);
+                        var o = Unwrap(types[i]);
+                        genTypes[i] = o as Type ?? (PythonType)o;
+                        if (genTypes[i] == null) {
+                            throw new InvalidOperationException("Cannot get type from " + o.GetType().FullName);
+                        }
                     }
-                    return MakeHandle(genType.Type.MakeGenericType(genTypes));
+                    return MakeHandle(DynamicHelpers.GetPythonTypeFromType(genType.Type.MakeGenericType(genTypes)));
                 }
                 return new ObjectIdentityHandle();
             });
