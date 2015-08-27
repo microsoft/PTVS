b9622aa8dcbc5ea6542d47ee69ecd80b770b181a
Steve Dower <steve.dower@microsoft.com>
2015-08-27 10:02:47 -0700

    Updates helper script for drop paths.

--- Python/Tests/InstallFromDrop.ps1
+++ Python/Tests/InstallFromDrop.ps1
@@ -1,16 +1,22 @@
 param (
-    [string] $drop,
-    [string] $local,
-    [string] $install_msi,
     [string] $vsversion,
     [switch] $remove
 )
 
+$drop = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$install_msi = "$drop\unsigned\PythonToolsInstaller.msi"
+
+$logs = mkdir "$drop\logs" -Force
+
+"Finding $install_msi"
+if (-not (Test-Path $install_msi)) {
+    "ABORTING: $install_msi could not be found."
+    throw "$install_msi could not be found."
+}
+
 $exitcode = 1
 
 $ErrorActionPreference = 'SilentlyContinue'
-
-#$start_time = (Get-Date).ToUniversalTime()
 
 $vsinstalldir = Split-Path (Split-Path (&{switch($vsversion) {
     "10.0" { $env:VS100COMNTOOLS }
@@ -19,77 +25,26 @@ $vsinstalldir = Split-Path (Split-Path (&{switch($vsversion) {
     "14.0" { $env:VS140COMNTOOLS }
 }}))
 
-$refreshdb = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\refreshdb.py"
-
-"Working in $local"
-pushd (mkdir $local -Force)
-
-if ($remove) {
-    "Removing TestSccPackage"
-    del "$vsinstalldir\Common7\IDE\CommonExtensions\Platform\TestSccPackage.*"
-
-    "Uninstalling $install_msi"
-    Start-Process -Wait msiexec -ArgumentList "/l*vx", "TestResults\uninstall.log", "/q", "/x", "$install_msi"
-
-#    "Collecting Application log events"
-#    Start-Process -Wait wevtutil -ArgumentList "export-log", "Application", `
-#        "TestResults\Application.evtx", `
-#        ("/q:*[System[TimeCreated[@SystemTime>='{0:s}Z']]]" -f ($start_time))
-
-    "Complete"
-    exit $exitcode
-}
-
-
-try {
-    "Clearing old files"
-    gci -Recurse -Attributes ReparsePoint | %{ & cmd /c rmdir "$($_.Fullname)" }
-    if (gci *) { del * -Recurse -Force; sleep 5 }
-    if (gci *) { del * -Recurse -Force; sleep 5 }
-    if (gci *) { del * -Recurse -Force -ErrorAction Stop }
-    "Copying MSIs from drop"
-    copy "$drop\UnsignedMsi" . -r -fo
-    "Copying skip verification scripts"
-    copy "$drop\*.reg" . -fo
-    "Copying test binaries"
-    copy "$drop\Tests" Tests -r -fo
-    "" | Out-File build.root
-
-    "Executing EnableSkipVerification.reg"
-    if (Test-Path "$env:WinDir\Sysnative\cmd.exe") {
-        & "$env:WinDir\Sysnative\cmd.exe" /C "regedit.exe /s EnableSkipVerification.reg"
-    } else {
-        regedit.exe /s EnableSkipVerification.reg
-    }
-
-    "Restarting msiserver service"
-    if ((Get-Service msiserver).Status -ne 'Stopped') { (Get-Service msiserver).Stop(); sleep 1; }
-    if ((Get-Service msiserver).Status -ne 'Stopped') { (Get-Service msiserver).Stop(); sleep 1; }
-    if ((Get-Service msiserver).Status -ne 'Stopped') { (Get-Service msiserver).Stop(); sleep 1; }
-    if ((Get-Service msiserver).Status -ne 'Stopped') { throw "Failed to stop msiserver" }
-
-    "Looking for $install_msi"
-    if (-not (Test-Path $install_msi)) {
-        "ABORTING: $install_msi could not be found."
-        throw "$install_msi could not be found."
-    }
-
+if (-not $remove) {
     "Deploying TestSccPackage"
-    copy "$drop\Tests\TestSccPackage.*" (mkdir "$vsinstalldir\Common7\IDE\CommonExtensions\Platform" -Force) -Force
+    copy "$drop\test\TestSccPackage.*" (mkdir "$vsinstalldir\Common7\IDE\CommonExtensions\Platform" -Force) -Force
 
     "Installing $install_msi"
-    Start-Process -Wait msiexec -ArgumentList "/l*vx", "TestResults\install.log", "/q", "/i", "$install_msi"
+    Start-Process -Wait msiexec -ArgumentList "/l*vx", "$logs\install.log", "/q", "/i", "$install_msi"
     if ((gwmi win32_product -Filter "name like 'Python Tools %'").Count -eq 0) {
         "ABORTING: Failed to install"
         throw "Failed to install"
     }
-    
+
     "Refreshing Completion DB"
     "$((gp HKLM:\Software\Wow6432Node\IronPython\2.7\InstallPath\).'(default)')\ipy.exe" "$drop\Tests\refreshdb.py"
+} else {
+    "Removing TestSccPackage"
+    del "$vsinstalldir\Common7\IDE\CommonExtensions\Platform\TestSccPackage.*"
 
-    exit $exitcode
-} finally {
-    popd
+    "Uninstalling $install_msi"
+    Start-Process -Wait msiexec -ArgumentList "/l*vx", "$logs\uninstall.log", "/q", "/x", "$install_msi"
 }
 
+"Complete"
 exit $exitcode
