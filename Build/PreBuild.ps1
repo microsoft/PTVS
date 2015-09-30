f4b1789369c186397d1c5dc43efc9186152fd71d
Steve Dower <steve.dower@microsoft.com>
2015-09-30 10:40:33 -0700

    Fixes PreBuild and PowershellEnvironment scripts.

--- Build/PreBuild.ps1
+++ Build/PreBuild.ps1
@@ -1,4 +1,5 @@
-$buildroot = (Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Definition))
-pushd "$buildroot\BuildOutput"
-& "$buildroot\Build\nuget.exe" install "$buildroot\Build\packages.config"
+"Restoring Packages"
+$buildroot = $MyInvocation.MyCommand.Definition | Split-Path -Parent | Split-Path -Parent
+pushd "$buildroot\Build"
+.\nuget.exe restore packages.config -PackagesDirectory "$buildroot\BuildOutput"
 popd
