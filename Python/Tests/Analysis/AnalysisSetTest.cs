2b386f61e79ac9172ba71a88b9ab4b8564113611
Steve Dower <steve.dower@microsoft.com>
2015-10-26 12:04:45 -0700

    Splits out AttachTests from DebuggerTests Renames NamespaceSetTest to AnalysisSetTest.

--- Python/Tests/Analysis/AnalysisSetTest.cs
+++ Python/Tests/Analysis/AnalysisSetTest.cs
@@ -19,7 +19,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestUtilities;
 
 namespace AnalysisTests {
-    class TestNamespace : AnalysisValue {
+    class TestAnalysisValue : AnalysisValue {
         public string _name;
         public override string Name {
             get { return _name; }
@@ -27,12 +27,12 @@ namespace AnalysisTests {
         public string Value;
         public int MergeCount;
 
-        public TestNamespace() {
+        public TestAnalysisValue() {
             MergeCount = 1;
         }
 
         public override bool Equals(object obj) {
-            var tns = obj as TestNamespace;
+            var tns = obj as TestAnalysisValue;
             if (tns == null) {
                 return false;
             }
@@ -44,7 +44,7 @@ namespace AnalysisTests {
         }
 
         internal override bool UnionEquals(AnalysisValue ns, int strength) {
-            var tns = ns as TestNamespace;
+            var tns = ns as TestAnalysisValue;
             if (tns == null) {
                 return false;
             }
@@ -56,11 +56,11 @@ namespace AnalysisTests {
         }
 
         internal override AnalysisValue UnionMergeTypes(AnalysisValue ns, int strength) {
-            var tns = ns as TestNamespace;
+            var tns = ns as TestAnalysisValue;
             if (tns == null || object.ReferenceEquals(this, tns)) {
                 return this;
             }
-            return new TestNamespace { _name = Name, Value = MergeCount > tns.MergeCount ? Value : tns.Value, MergeCount = MergeCount + tns.MergeCount };
+            return new TestAnalysisValue { _name = Name, Value = MergeCount > tns.MergeCount ? Value : tns.Value, MergeCount = MergeCount + tns.MergeCount };
         }
 
         public override string ToString() {
@@ -69,29 +69,29 @@ namespace AnalysisTests {
     }
 
     [TestClass]
-    public class NamespaceSetTest {
-        private static readonly AnalysisValue nsA1 = new TestNamespace { _name = "A", Value = "a" };
-        private static readonly AnalysisValue nsA2 = new TestNamespace { _name = "A", Value = "x" };
+    public class AnalysisSetTest {
+        private static readonly AnalysisValue nsA1 = new TestAnalysisValue { _name = "A", Value = "a" };
+        private static readonly AnalysisValue nsA2 = new TestAnalysisValue { _name = "A", Value = "x" };
         private static readonly AnalysisValue nsAU1 = nsA1.UnionMergeTypes(nsA2, 100);
         private static readonly AnalysisValue nsAU2 = nsAU1.UnionMergeTypes(nsA2, 100);
         private static readonly AnalysisValue nsAU3 = nsAU2.UnionMergeTypes(nsA2, 100);
         private static readonly AnalysisValue nsAU4 = nsAU3.UnionMergeTypes(nsA2, 100);
 
-        private static readonly AnalysisValue nsB1 = new TestNamespace { _name = "B", Value = "b" };
-        private static readonly AnalysisValue nsB2 = new TestNamespace { _name = "B", Value = "y" };
+        private static readonly AnalysisValue nsB1 = new TestAnalysisValue { _name = "B", Value = "b" };
+        private static readonly AnalysisValue nsB2 = new TestAnalysisValue { _name = "B", Value = "y" };
         private static readonly AnalysisValue nsBU1 = nsB1.UnionMergeTypes(nsB2, 100);
         private static readonly AnalysisValue nsBU2 = nsBU1.UnionMergeTypes(nsB2, 100);
         private static readonly AnalysisValue nsBU3 = nsBU2.UnionMergeTypes(nsB2, 100);
         private static readonly AnalysisValue nsBU4 = nsBU3.UnionMergeTypes(nsB2, 100);
 
-        private static readonly AnalysisValue nsC1 = new TestNamespace { _name = "C", Value = "c" };
-        private static readonly AnalysisValue nsC2 = new TestNamespace { _name = "C", Value = "z" };
+        private static readonly AnalysisValue nsC1 = new TestAnalysisValue { _name = "C", Value = "c" };
+        private static readonly AnalysisValue nsC2 = new TestAnalysisValue { _name = "C", Value = "z" };
         private static readonly AnalysisValue nsCU1 = nsC1.UnionMergeTypes(nsC2, 100);
         private static readonly AnalysisValue nsCU2 = nsCU1.UnionMergeTypes(nsC2, 100);
         private static readonly AnalysisValue nsCU3 = nsCU2.UnionMergeTypes(nsC2, 100);
         private static readonly AnalysisValue nsCU4 = nsCU3.UnionMergeTypes(nsC2, 100);
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void SetOfOne_Object() {
             var set = AnalysisSet.Create(nsA1);
             Assert.AreSame(nsA1, set);
@@ -106,7 +106,7 @@ namespace AnalysisTests {
             Assert.AreNotSame(nsA1, set);
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void SetOfOne_Union() {
             var set = AnalysisSet.CreateUnion(nsA1, UnionComparer.Instances[0]);
             Assert.IsInstanceOfType(set, typeof(AnalysisSetOneUnion));
@@ -125,7 +125,7 @@ namespace AnalysisTests {
             AssertUtil.ContainsExactly(set, nsAU1);
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void SetOfTwo_Object() {
             var set = AnalysisSet.Create(new[] { nsA1, nsA2 });
             Assert.IsInstanceOfType(set, typeof(AnalysisSetTwoObject));
@@ -136,7 +136,7 @@ namespace AnalysisTests {
             AssertUtil.ContainsExactly(set, nsA1, nsA2);
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void SetOfTwo_Union() {
             var set = AnalysisSet.CreateUnion(new[] { nsA1, nsA2, nsB1, nsB2 }, UnionComparer.Instances[0]);
             Assert.IsInstanceOfType(set, typeof(AnalysisSetTwoUnion));
@@ -147,7 +147,7 @@ namespace AnalysisTests {
             AssertUtil.ContainsExactly(set, nsAU2, nsBU2);
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void ManySet_Object() {
             var set = AnalysisSet.Create(new[] { nsA1, nsA2, nsB1, nsB2 });
             Assert.IsInstanceOfType(set, typeof(AnalysisHashSet));
@@ -160,7 +160,7 @@ namespace AnalysisTests {
             AssertUtil.ContainsExactly(set, nsA1, nsA2, nsB1, nsB2);
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void ManySet_Union() {
             var set = AnalysisSet.CreateUnion(new[] { nsA1, nsA2, nsB1, nsB2, nsC1 }, UnionComparer.Instances[0]);
             Assert.IsInstanceOfType(set, typeof(AnalysisHashSet));
@@ -175,7 +175,7 @@ namespace AnalysisTests {
 
 
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void EmptySet_Add_Object() {
             var set = AnalysisSet.Empty;
             Assert.IsInstanceOfType(set, typeof(AnalysisSetEmptyObject));
@@ -195,7 +195,7 @@ namespace AnalysisTests {
             Assert.AreSame(nsA1, set);
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void EmptySet_Add_Union() {
             var set = AnalysisSet.EmptyUnion;
             Assert.IsInstanceOfType(set, typeof(AnalysisSetEmptyUnion));
@@ -215,7 +215,7 @@ namespace AnalysisTests {
             AssertUtil.ContainsExactly(set, nsA1);
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void SetOfOne_Add_Object() {
             var set = AnalysisSet.Create(nsA1);
 
@@ -239,7 +239,7 @@ namespace AnalysisTests {
             AssertUtil.ContainsExactly(set2, nsA1);
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void SetOfOne_Add_Union() {
             var set = AnalysisSet.CreateUnion(nsA1, UnionComparer.Instances[0]);
 
@@ -264,7 +264,7 @@ namespace AnalysisTests {
             AssertUtil.ContainsExactly(set2, nsAU1);
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void SetOfTwo_Add_Object() {
             var set = AnalysisSet.Create(new[] { nsA1, nsB1 });
             IAnalysisSet set2;
@@ -293,7 +293,7 @@ namespace AnalysisTests {
             }
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void SetOfTwo_Add_Union() {
             var set = AnalysisSet.CreateUnion(new[] { nsA1, nsB1 }, UnionComparer.Instances[0]);
             IAnalysisSet set2;
@@ -334,7 +334,7 @@ namespace AnalysisTests {
             Assert.IsTrue(added);
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void ManySet_Add_Object() {
             var set = AnalysisSet.Create(new[] { nsA1, nsB1, nsC1 });
             IAnalysisSet set2;
@@ -364,7 +364,7 @@ namespace AnalysisTests {
             }
         }
 
-        [TestMethod, TestCategory("AnalysisSet")]
+        [TestMethod, Priority(1)]
         public void ManySet_Add_Union() {
             var set = AnalysisSet.CreateUnion(new[] { nsA1, nsB1, nsC1 }, UnionComparer.Instances[0]);
             IAnalysisSet set2;
