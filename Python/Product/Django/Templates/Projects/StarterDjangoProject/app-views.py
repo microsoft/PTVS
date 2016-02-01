86e1906c5e09c308430f9893ff1bae52e84deb14
Steve Dower <steve.dower@microsoft.com>
2016-02-01 13:54:34 -0800

    Fixes RequestContext deprecation warning Adds 1.9 app template and makes it the default

--- Python/Product/Django/Templates/Projects/StarterDjangoProject/app-views.py
+++ Python/Product/Django/Templates/Projects/StarterDjangoProject/app-views.py
@@ -13,11 +13,10 @@ def home(request):
     return render(
         request,
         'app/index.html',
-        context_instance = RequestContext(request,
         {
             'title':'Home Page',
             'year':datetime.now().year,
-        })
+        }
     )
 
 def contact(request):
@@ -26,12 +25,11 @@ def contact(request):
     return render(
         request,
         'app/contact.html',
-        context_instance = RequestContext(request,
         {
             'title':'Contact',
             'message':'Your contact page.',
             'year':datetime.now().year,
-        })
+        }
     )
 
 def about(request):
@@ -40,10 +38,9 @@ def about(request):
     return render(
         request,
         'app/about.html',
-        context_instance = RequestContext(request,
         {
             'title':'About',
             'message':'Your application description page.',
             'year':datetime.now().year,
-        })
+        }
     )
