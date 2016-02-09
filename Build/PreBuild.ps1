6e6b4aadc678facaf45cd40f4f8a2ec7a1e42070
Steve Dower <steve.dower@microsoft.com>
2016-02-09 10:57:28 -0800

    Updates prebuild.ps1

--- Build/PreBuild.ps1
+++ Build/PreBuild.ps1
@@ -1,5 +1,12 @@
+param ($packages, $source)
 "Restoring Packages"
 $buildroot = $MyInvocation.MyCommand.Definition | Split-Path -Parent | Split-Path -Parent
 pushd "$buildroot\Build"
-.\nuget.exe restore -PackagesDirectory "$buildroot\BuildOutput" $args
+if ($source) {
+    .\nuget.exe sources add -Name PreBuildSource -Source $source
+}
+.\nuget.exe restore $packages -PackagesDirectory "$buildroot\BuildOutput"
+if ($source) {
+    .\nuget.exe sources remove -Name PreBuildSource
+}
 popd
