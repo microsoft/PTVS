1b44667e0e27743da334b7b9d682248b10bb2f0f
Steve Dower <steve.dower@microsoft.com>
2016-02-10 10:23:12 -0800

    Workaround repl attribute

--- Python/Product/PythonTools/PythonToolsPackage.cs
+++ Python/Product/PythonTools/PythonToolsPackage.cs
@@ -186,7 +186,7 @@ namespace Microsoft.PythonTools {
     [ProvideDiffSupportedContentType(".py;.pyw", ";")]
     [ProvideCodeExpansions(GuidList.guidPythonLanguageService, false, 106, "Python", @"Snippets\%LCID%\SnippetsIndex.xml", @"Snippets\%LCID%\Python\")]
     [ProvideCodeExpansionPath("Python", "Test", @"Snippets\%LCID%\Test\")]
-    [ProvideInteractiveWindow(GuidList.guidPythonInteractiveWindow, Style = VsDockStyle.Linked, Orientation = ToolWindowOrientation.none, Window = ToolWindowGuids80.Outputwindow)]
+    //[ProvideInteractiveWindow(GuidList.guidPythonInteractiveWindow, Style = VsDockStyle.Linked, Orientation = ToolWindowOrientation.none, Window = ToolWindowGuids80.Outputwindow)]
     [SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable",
         Justification = "Object is owned by VS and cannot be disposed")]
     public sealed class PythonToolsPackage : CommonPackage, IVsComponentSelectorProvider, IPythonToolsToolWindowService {
