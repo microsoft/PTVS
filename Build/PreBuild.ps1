d10cff7272e07480d7571a4e6494c6f9ee336c89
Steve Dower <steve.dower@microsoft.com>
2016-02-11 14:02:28 -0800

    Makes Nuget install packages to BuildOutputRoot consistently.

--- Build/PreBuild.ps1
+++ Build/PreBuild.ps1
@@ -1,11 +1,16 @@
-param ($packages, $source)
+param ($config, $source)
 "Restoring Packages"
 $buildroot = $MyInvocation.MyCommand.Definition | Split-Path -Parent | Split-Path -Parent
 pushd "$buildroot\Build"
 if ($source) {
     .\nuget.exe sources add -Name PreBuildSource -Source $source
 }
-.\nuget.exe restore $packages -PackagesDirectory "$buildroot\BuildOutput"
+if ($env:TF_BUILD_BinariesDirectory) {
+    $packages = "${env:TF_BUILD_BinariesDirectory}"
+} else {
+    $packages = "$buildroot\BuildOutput"
+}
+.\nuget.exe restore $config -PackagesDirectory $packages
 if ($source) {
     .\nuget.exe sources remove -Name PreBuildSource
 }
