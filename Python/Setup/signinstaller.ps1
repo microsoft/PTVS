c6e8cb8d0db97802a706b682d288b7fd9013e0cc
Steve Dower <steve.dower@microsoft.com>
2016-02-12 15:58:41 -0800

    Fixes signing script

--- Python/Setup/signinstaller.ps1
+++ Python/Setup/signinstaller.ps1
@@ -8,16 +8,16 @@ if ($mock) {
 
 $AuthenticodeFiles = @(
     "PythonToolsInstaller.msi"
-) | %{ @{path="$build\setup\en-us\$_"; name="$_"} } | ?{ Test-Path "$($_.path)" }
+) | %{ @{path="$build\raw\setup\en-us\$_"; name="$_"} } | ?{ Test-Path "$($_.path)" }
 
 $ErrorActionPreference = "Stop"
 
 $jobs = @()
 $jobs += begin_sign_files $AuthenticodeFiles `
-         (mkdir "$build\setup\signed" -Force) @("stevdo", "dinov") `
+         (mkdir "$build\raw\setup\signed" -Force) @("stevdo", "dinov") `
          "Python Tools for Visual Studio" "http://aka.ms/ptvs" "Python Tools for Visual Studio" "python;visual studio" `
          "authenticode"
 
 end_sign_files $jobs
 
-copy "$build\setup\signed\PythonToolsInstaller.msi" "$build\release\PTVS$buildidentifier.msi"
+copy "$build\raw\setup\signed\PythonToolsInstaller.msi" "$build\release\PTVS$buildidentifier.msi"
