b5bffb5eea49a5649cedbcc98cd820cec99cec2f
Steve Dower <steve.dower@microsoft.com>
2016-02-12 15:49:26 -0800

    Adds sign installer script.

---
+++ Python/Setup/signinstaller.ps1
@@ -0,0 +1,23 @@
+param($root, $build, $buildidentifier, [switch] $mock)
+
+if ($mock) {
+    Import-Module "$root\Build\BuildReleaseMockHelpers.psm1" -Force
+} else {
+    Import-Module "$root\Build\BuildReleaseHelpers.psm1" -Force
+}
+
+$AuthenticodeFiles = @(
+    "PythonToolsInstaller.msi"
+) | %{ @{path="$build\setup\en-us\$_"; name="$_"} } | ?{ Test-Path "$($_.path)" }
+
+$ErrorActionPreference = "Stop"
+
+$jobs = @()
+$jobs += begin_sign_files $AuthenticodeFiles `
+         (mkdir "$build\setup\signed" -Force) @("stevdo", "dinov") `
+         "Python Tools for Visual Studio" "http://aka.ms/ptvs" "Python Tools for Visual Studio" "python;visual studio" `
+         "authenticode"
+
+end_sign_files $jobs
+
+copy "$build\setup\signed\PythonToolsInstaller.msi" "$build\release\PTVS$buildidentifier.msi"
