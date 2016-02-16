441a91d47941a0c4e436c2294018e047c446fc90
Steve Dower <steve.dower@microsoft.com>
2016-02-16 11:46:24 -0800

    Fixes signing script for installer.

--- Python/Setup/signinstaller.ps1
+++ Python/Setup/signinstaller.ps1
@@ -6,18 +6,19 @@ if ($mock) {
     Import-Module "$root\Build\BuildReleaseHelpers.psm1" -Force
 }
 
-$AuthenticodeFiles = @(
+$MsiFiles = @(@(
     "PythonToolsInstaller.msi"
-) | %{ @{path="$build\raw\setup\en-us\$_"; name="$_"} } | ?{ Test-Path "$($_.path)" }
+) | %{ @{path="$build\raw\setup\en-us\$_"; name="$_"} } | ?{ Test-Path "$($_.path)" })
 
 $ErrorActionPreference = "Stop"
 
 $jobs = @()
-$jobs += begin_sign_files $AuthenticodeFiles `
+$jobs += begin_sign_files $MsiFiles `
          (mkdir "$build\raw\setup\signed" -Force) @("stevdo", "dinov") `
          "Python Tools for Visual Studio" "http://aka.ms/ptvs" "Python Tools for Visual Studio" "python;visual studio" `
          "msi"
 
 end_sign_files $jobs
 
+mkdir "$build\release" -Force
 copy "$build\raw\setup\signed\PythonToolsInstaller.msi" "$build\release\PTVS$buildidentifier.msi"
