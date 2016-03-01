c29f677c0964500ad085e630e3a71a5bd40737fa
Steve Dower <steve.dower@microsoft.com>
2016-03-01 11:56:59 -0800

    Moves templates under group names and checks in shared files to make it easy to generate separate packages.

---
+++ Python/Templates/SharedFiles/update_templates.ps1
@@ -0,0 +1,28 @@
+#
+# Overwrites the common files in certain templates based on the source files
+# in this directory. Modifications should be checked in.
+#
+
+$web_role_common = @('ps.cmd', 'ConfigureCloudService.ps1', 'WebRoleConfiguration.mht') | %{ gi $_ }
+$worker_role_common = @('ps.cmd', 'ConfigureCloudService.ps1', 'LaunchWorker.ps1', 'WorkerRoleConfiguration.mht') | %{ gi $_ }
+
+$web_role_targets = @(
+    "..\Web\ItemTemplates\CloudService\NETFramework4\Web Role\Python\CloudServiceBottleWebRole",
+    "..\Django\ItemTemplates\CloudService\NETFramework4\Web Role\Python\CloudServiceDjangoWebRole",
+    "..\Web\ItemTemplates\CloudService\NETFramework4\Web Role\Python\CloudServiceEmptyWebRole",
+    "..\Web\ItemTemplates\CloudService\NETFramework4\Web Role\Python\CloudServiceFlaskWebRole",
+    "..\Web\ItemTemplates\Python\AzureCSWebRole",
+    "..\Web\ProjectTemplates\Python\WebRoleBottle",
+    "..\Django\ProjectTemplates\Python\WebRoleDjango",
+    "..\Web\ProjectTemplates\Python\WebRoleEmpty",
+    "..\Web\ProjectTemplates\Python\WebRoleFlask"
+) | %{ gi $_ }
+
+$worker_role_targets = @(
+    "..\Web\ItemTemplates\CloudService\NETFramework4\Worker Role\Python\CloudServiceWorkerRole",
+    "..\Web\ItemTemplates\Python\AzureCSWorkerRole",
+    "..\Web\ProjectTemplates\Python\WorkerRoleProject"
+) | %{ gi $_ }
+
+$web_role_targets | %{ copy $web_role_common $_ }
+$worker_role_targets | %{ copy $worker_role_common $_ }
---
+++ Python/Templates/Web/ProjectTemplates/Python/WorkerRoleProject/ps.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+cd /D %~dp0
+
+if not exist "%DiagnosticStore%\LogFiles" mkdir "%DiagnosticStore%\LogFiles"
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -File %* >> "%DiagnosticStore%\LogFiles\%~n1.txt" 2>> "%DiagnosticStore%\LogFiles\%~n1.err.txt"
