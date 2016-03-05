68d4aed7e2181f404f194c62bf1e4870aebe145d
dino <dinov@microsoft.com>
2016-03-04 17:59:32 -0800

    Add missing files and project update

---
+++ Python/Product/PythonTools/PythonTools/Intellisense/AnalysisLocation.cs
@@ -0,0 +1,98 @@
+﻿// Python Tools for Visual Studio
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.PythonTools.Intellisense {
+    /// <summary>
+    /// Represents a location that comes out of an analysis.  Includes a file, line, and column
+    /// number.
+    /// </summary>
+    public sealed class AnalysisLocation : IEquatable<AnalysisLocation> {
+        internal readonly AnalysisEntry File;
+        public readonly int Line, Column;
+        private static readonly IEqualityComparer<AnalysisLocation> _fullComparer = new FullLocationComparer();
+
+        internal AnalysisLocation(AnalysisEntry file, int line, int column) {
+            File = file;
+            Line = line;
+            Column = column;
+        }
+
+        public string FilePath {
+            get {
+                return File.Path;
+            }
+        }
+
+        internal void GotoSource(IServiceProvider serviceProvider) {
+            string zipFileName = VsProjectAnalyzer.GetZipFileName(File);
+            if (zipFileName == null) {
+                PythonToolsPackage.NavigateTo(
+                    serviceProvider,
+                    File.Path,
+                    Guid.Empty,
+                    Line - 1,
+                    Column - 1
+                );
+            }
+        }
+
+
+        public override bool Equals(object obj) {
+            AnalysisLocation other = obj as AnalysisLocation;
+            if (other != null) {
+                return Equals(other);
+            }
+            return false;
+        }
+
+        public override int GetHashCode() {
+            return Line.GetHashCode() ^ File.GetHashCode();
+        }
+
+        public bool Equals(AnalysisLocation other) {
+            // currently we filter only to line & file - so we'll only show 1 ref per each line
+            // This works nicely for get and call which can both add refs and when they're broken
+            // apart you still see both refs, but when they're together you only see 1.
+            return Line == other.Line &&
+                File == other.File;
+        }
+
+        /// <summary>
+        /// Provides an IEqualityComparer that compares line, column and project entries.  By
+        /// default locations are equaitable based upon only line/project entry.
+        /// </summary>
+        public static IEqualityComparer<AnalysisLocation> FullComparer {
+            get {
+                return _fullComparer;
+            }
+        }
+
+        sealed class FullLocationComparer : IEqualityComparer<AnalysisLocation> {
+            public bool Equals(AnalysisLocation x, AnalysisLocation y) {
+                return x.Line == y.Line &&
+                    x.Column == y.Column &&
+                    x.File == y.File;
+            }
+
+            public int GetHashCode(AnalysisLocation obj) {
+                return obj.Line.GetHashCode() ^ obj.Column.GetHashCode() ^ obj.File.GetHashCode();
+            }
+        }
+    }
+}
---
+++ Python/Product/PythonTools/PythonTools/Intellisense/ExpressionAnalysis.cs
@@ -0,0 +1,72 @@
+﻿// Python Tools for Visual Studio
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+using Microsoft.VisualStudio.Text;
+
+namespace Microsoft.PythonTools.Intellisense {
+    /// <summary>
+    /// Represents information about an analyzed expression.  This is returned from 
+    /// AnalyzeExpression which is defined as an extension method in <see cref="Microsoft.PythonTools.Intellisense.PythonAnalysisExtensions"/>
+    /// </summary>
+    public sealed class ExpressionAnalysis {
+        private readonly string _expr;
+        private readonly ITrackingSpan _span;
+        private readonly AnalysisVariable[] _variables;
+        private readonly VsProjectAnalyzer _analyzer;
+        private readonly string _privatePrefix;
+        private readonly string _memberName;
+
+        internal ExpressionAnalysis(string text, ITrackingSpan span, AnalysisVariable[] variables, string privatePrefix, string memberName) {
+            _span = span;
+            _expr = text;
+            _variables = variables;
+            _privatePrefix = privatePrefix;
+            _memberName = memberName;
+        }
+
+        /// <summary>
+        /// Gets the expression which was evaluated to create this expression analyze.
+        /// 
+        /// An expression analysis is usually created from a point in a buffer and this
+        /// is the complete expression that the point mapped to.
+        /// </summary>
+        public string Expression => _expr;
+        
+        /// <summary>
+        /// Gets the span associated with the expression.
+        /// </summary>
+        public ITrackingSpan Span => _span;
+        
+        /// <summary>
+        /// Gets the list of variables which the expression refers to.  This can include
+        /// references and definitions for variables, fields, etc... as well as actual
+        /// values stored in those fields.
+        /// </summary>
+        public AnalysisVariable[] Variables => _variables;
+
+        /// <summary>
+        /// Gets the private prefix for this expression analysis.  This will be set
+        /// when inside of a class where names could be mangled.
+        /// </summary>
+        public string PrivatePrefix => _privatePrefix;
+
+        /// <summary>
+        /// If the expression is a member name (e.g. hello.world, or hello.good.world)
+        /// this gets the name of the member such as "hello".
+        /// </summary>
+        public string MemberName => _memberName;
+    }
+}
