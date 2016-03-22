9fd1051214b4deab759320115aaa8c6182a1a652
Steve Dower <steve.dower@microsoft.com>
2016-03-22 14:50:01 -0700

    [WIP] Updates build and signing to work with internal build system. Makes prerequisites script run on build if necessary. Suppresses unnecessary warning in templates project.

--- Build/PreBuild.ps1
+++ Build/PreBuild.ps1
@@ -1,16 +1,44 @@
-param ($config, $source)
+param ($vstarget, $source, [switch] $clean)
+
+# This is the list of packages we require to build, and the version to use for each supported $vstarget
+$packages = @(
+    @{ name="Microsoft.VSSDK.BuildTools"; version=@{ "14.0"="14.0.23205"; "15.0"="15.0.25001-Dev15CTP1" }; required=$true },
+    @{ name="Newtonsoft.Json"; version=@{ "14.0"="6.0.8"; "15.0"="6.0.8" }; required=$true },
+    @{ name="VS.Tools.Setup.FopCreator"; version=@{ "14.0"="1.0.16022601"; "15.0"="1.0.16022601" }; required=$false }
+)
+
+
 "Restoring Packages"
+
+if ($vstarget.ToString() -match "^\d\d$") {
+    $vstarget = "$vstarget.0"
+}
+
 $buildroot = $MyInvocation.MyCommand.Definition | Split-Path -Parent | Split-Path -Parent
 pushd "$buildroot\Build"
 if ($source) {
     .\nuget.exe sources add -Name PreBuildSource -Source $source
 }
 if ($env:TF_BUILD_BinariesDirectory) {
-    $packages = "${env:TF_BUILD_BinariesDirectory}"
+    $outdir = "${env:TF_BUILD_BinariesDirectory}"
 } else {
-    $packages = "$buildroot\BuildOutput"
+    $outdir = "$buildroot\BuildOutput"
 }
-.\nuget.exe restore $config -PackagesDirectory $packages
+
+
+if ($clean) {
+    $packages.name | %{ rmdir -r -fo -ea 0 "$outdir\$_" }
+}
+
+$packages | %{
+    $arglist = "install", $_.name, "-Version", $_.version[$vstarget], "-ExcludeVersion", "-OutputDirectory", $outdir
+    if ($_.required) {
+        Start-Process -Wait -NoNewWindow .\nuget.exe -ErrorAction Stop -ArgumentList $arglist
+    } else {
+        Start-Process -Wait -NoNewWindow .\nuget.exe -ErrorAction Continue -ArgumentList $arglist
+    }
+}
+
 if ($source) {
     .\nuget.exe sources remove -Name PreBuildSource
 }
