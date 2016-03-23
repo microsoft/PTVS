ea0b4793431a8cc26cc413096ba5db4e0b5186c1
Steve Dower <steve.dower@microsoft.com>
2016-03-23 09:44:12 -0700

    Updates VSSDK version for Dev15.

--- Build/PreBuild.ps1
+++ Build/PreBuild.ps1
@@ -2,7 +2,7 @@ param ($vstarget, $source, [switch] $clean)
 
 # This is the list of packages we require to build, and the version to use for each supported $vstarget
 $packages = @(
-    @{ name="Microsoft.VSSDK.BuildTools"; version=@{ "14.0"="14.0.23205"; "15.0"="15.0.25001-Dev15CTP1" }; required=$true },
+    @{ name="Microsoft.VSSDK.BuildTools"; version=@{ "14.0"="14.0.23205"; "15.0"="15.0.25022-Dev15CTP1" }; required=$true },
     @{ name="Newtonsoft.Json"; version=@{ "14.0"="6.0.8"; "15.0"="6.0.8" }; required=$true },
     @{ name="VS.Tools.Setup.FopCreator"; version=@{ "14.0"="1.0.16022601"; "15.0"="1.0.16022601" }; required=$false },
     @{ name="MicroBuild.Core"; version=@{ "14.0"="0.2.0"; "15.0"="0.2.0" }; required=$false }
