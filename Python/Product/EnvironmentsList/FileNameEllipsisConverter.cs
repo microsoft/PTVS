92a916b39e39f84d93b2459a62b5343524a99367
Steve Dower <steve.dower@microsoft.com>
2016-04-11 10:53:29 -0700

    Adds tests and fixes implementation to handle more edge cases.

--- Python/Product/EnvironmentsList/FileNameEllipsisConverter.cs
+++ Python/Product/EnvironmentsList/FileNameEllipsisConverter.cs
@@ -17,11 +17,17 @@
 using System;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Windows.Data;
 
 namespace Microsoft.PythonTools.EnvironmentsList {
     [ValueConversion(typeof(string), typeof(string))]
     sealed class FileNameEllipsisConverter : IValueConverter {
+        private static readonly char[] PathSeparators = new [] {
+            Path.DirectorySeparatorChar,
+            Path.AltDirectorySeparatorChar
+        };
+
         public bool IncludeHead { get; set; }
         public bool IncludeBody { get; set; }
         public bool IncludeTail { get; set; }
@@ -32,15 +38,26 @@ namespace Microsoft.PythonTools.EnvironmentsList {
                 return path;
             }
 
-            var headSplit = path.IndexOf('\\') + 1;
-            var headSplit2 = path.IndexOf('/') + 1;
-            if (headSplit > 0 && headSplit2 > 0 && headSplit2 < headSplit) {
-                headSplit = headSplit2;
+            var headSplit = path.IndexOfAny(PathSeparators) + 1;
+            if (headSplit == path.Length) {
+                headSplit = -1;
             }
-            var tailSplit = Math.Max(path.LastIndexOf('\\'), path.LastIndexOf('/'));
+
+            var tailSplit = path.LastIndexOfAny(PathSeparators);
+            if (tailSplit == path.Length - 1) {
+                if (path.Length > 1) {
+                    tailSplit = path.LastIndexOfAny(PathSeparators, tailSplit - 1);
+                } else {
+                    tailSplit = 0;
+                }
+            }
+
+            if (headSplit == tailSplit + 1) {
+                headSplit -= 1;
+            }
 
             var head = (headSplit > 0) ? path.Remove(headSplit) : string.Empty;
-            var tail = (tailSplit > 0) ? path.Substring(tailSplit) : string.Empty;
+            var tail = (tailSplit > 0) ? path.Substring(tailSplit) : path;
 
             var body = string.Empty;
             if (tailSplit > headSplit) {
