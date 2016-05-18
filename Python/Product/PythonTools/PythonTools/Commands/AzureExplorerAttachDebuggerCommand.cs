86fc153772cd877eed1d494ea65ac56244b9bc94
Steve Dower <steve.dower@microsoft.com>
2016-05-18 16:06:09 -0700

    Remove Dev14 limitation.

--- Python/Product/PythonTools/PythonTools/Commands/AzureExplorerAttachDebuggerCommand.cs
+++ Python/Product/PythonTools/PythonTools/Commands/AzureExplorerAttachDebuggerCommand.cs
@@ -14,8 +14,6 @@
 // See the Apache Version 2.0 License for specific language governing
 // permissions and limitations under the License.
 
-#if DEV14
-
 using System;
 using System.Net;
 using System.Runtime.InteropServices;
@@ -207,5 +205,3 @@ namespace Microsoft.PythonTools.Commands {
         }
     }
 }
-
-#endif
