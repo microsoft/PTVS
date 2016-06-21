534660555528ae027d4b012f0a429fe46baf2554
Steve Dower <steve.dower@microsoft.com>
2016-06-21 11:18:13 -0700

    Removes unnecessary package dependency.

--- Build/PreBuild.ps1
+++ Build/PreBuild.ps1
@@ -4,7 +4,6 @@ param ($vstarget, $source, [switch] $clean, [switch] $full)
 $packages = @(
     @{ name="Microsoft.VSSDK.BuildTools"; version=@{ "14.0"="14.0.23205"; "15.0"="15.0.25022-Dev15CTP1" }; required=$true },
     @{ name="Newtonsoft.Json"; version=@{ "14.0"="6.0.8"; "15.0"="6.0.8" }; required=$true },
-    @{ name="VS.Tools.Setup.FopCreator"; version=@{ "14.0"="1.0.16022601"; "15.0"="1.0.16022601" }; required=$false },
     @{ name="MicroBuild.Core"; version=@{ "14.0"="0.2.0"; "15.0"="0.2.0" }; required=$false }
 )
 
