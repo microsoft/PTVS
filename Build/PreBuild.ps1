69c4b80bdc0a2ece81747c65e894e3228c97f597
Steve Dower <steve.dower@microsoft.com>
2016-06-23 13:54:41 -0700

    Adds Shell.Interop.12.1.DesignTime to installed packages.

--- Build/PreBuild.ps1
+++ Build/PreBuild.ps1
@@ -6,6 +6,7 @@ $packages = @(
     @{ name="Newtonsoft.Json"; version=@{ "14.0"="6.0.8"; "15.0"="6.0.8" }; required=$true },
     @{ name="MicroBuild.Core"; version=@{ "14.0"="0.2.0"; "15.0"="0.2.0" }; required=$false },
     @{ name="Microsoft.VisualStudio.Imaging.Interop.14.0.DesignTime"; version=@{ "14.0"="14.2.25123"; "15.0"="14.2.25123" }; required=$true },
+    @{ name="Microsoft.VisualStudio.Shell.Interop.12.1.DesignTime"; version=@{ "14.0"="12.1.30328"; "15.0"="12.1.30328" }; required=$true },
     @{ name="Microsoft.VisualStudio.Shell.Interop.14.0.DesignTime"; version=@{ "14.0"="14.2.25123"; "15.0"="14.2.25123" }; required=$true }
 )
 
