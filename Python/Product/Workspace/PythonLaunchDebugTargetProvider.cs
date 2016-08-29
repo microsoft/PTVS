592f1c4a5371113f288d8686166170fc43a95b3c
Steve Dower <steve.dower@microsoft.com>
2016-08-29 14:26:11 -0700

    [WIP] Adds extra dependency and basic launch debug target provider.

---
+++ Python/Product/Workspace/PythonLaunchDebugTargetProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Workspace;
+using Microsoft.VisualStudio.Workspace.Debug;
+using Microsoft.VisualStudio.Workspace.Extensions.Build;
+
+namespace Microsoft.PythonTools.Workspace {
+    [ExportLaunchConfigurationProvider(
+        ProviderType,
+        new[] { PythonConstants.FileExtension, PythonConstants.WindowsFileExtension },
+        LaunchTypeName,
+        JsonSchema
+    )]
+    class PythonLaunchDebugTargetProvider : ILaunchConfigurationProvider {
+        public const string ProviderType = "CCA8088B-06BC-4AE7-8521-FC66628ABE13";
+        public const string LaunchTypeName = "Python";
+        public const string JsonSchema = @"";
+
+        public bool IsDebugLaunchActionSupported(DebugLaunchActionContext debugLaunchActionContext) {
+            return true;
+        }
+
+        public void CustomizeLaunchConfiguration(DebugLaunchActionContext debugLaunchActionContext, IPropertySettings launchSettings) {
+            
+        }
+    }
+}
