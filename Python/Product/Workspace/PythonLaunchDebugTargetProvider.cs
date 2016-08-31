ba471041282d26b2c4bc136a8ff8be7c2734e636
Steve Dower <steve.dower@microsoft.com>
2016-08-31 10:22:49 -0700

    [WIP] Adds debug implementation.

--- Python/Product/Workspace/PythonLaunchDebugTargetProvider.cs
+++ Python/Product/Workspace/PythonLaunchDebugTargetProvider.cs
@@ -1,30 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Python Tools for Visual Studio
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
 using Microsoft.VisualStudio.Workspace;
 using Microsoft.VisualStudio.Workspace.Debug;
-using Microsoft.VisualStudio.Workspace.Extensions.Build;
 
 namespace Microsoft.PythonTools.Workspace {
     [ExportLaunchConfigurationProvider(
         ProviderType,
         new[] { PythonConstants.FileExtension, PythonConstants.WindowsFileExtension },
-        LaunchTypeName,
-        JsonSchema
+        PythonDebugLaunchProvider.LaunchTypeName,
+        PythonDebugLaunchProvider.JsonSchema
     )]
     class PythonLaunchDebugTargetProvider : ILaunchConfigurationProvider {
         public const string ProviderType = "CCA8088B-06BC-4AE7-8521-FC66628ABE13";
-        public const string LaunchTypeName = "Python";
-        public const string JsonSchema = @"";
 
         public bool IsDebugLaunchActionSupported(DebugLaunchActionContext debugLaunchActionContext) {
             return true;
         }
 
         public void CustomizeLaunchConfiguration(DebugLaunchActionContext debugLaunchActionContext, IPropertySettings launchSettings) {
-            
+            launchSettings[PythonDebugLaunchProvider.InterpreterKey] = "(default)";
+            launchSettings[PythonDebugLaunchProvider.InterpreterArgumentsKey] = string.Empty;
+            launchSettings[PythonDebugLaunchProvider.ScriptArgumentsKey] = string.Empty;
+            launchSettings[PythonDebugLaunchProvider.NativeDebuggingKey] = false;
         }
     }
 }
---
+++ Python/Product/Workspace/VsPythonDebugLaunchProvider.cs
@@ -0,0 +1,131 @@
+﻿// Python Tools for Visual Studio
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.PythonTools.Infrastructure;
+using Microsoft.PythonTools.Interpreter;
+using Microsoft.PythonTools.Project;
+using Microsoft.PythonTools.Project.Web;
+using Microsoft.VisualStudio.Workspace;
+using Microsoft.VisualStudio.Workspace.Debug;
+using Microsoft.VisualStudioTools.Project;
+
+namespace Microsoft.PythonTools.Workspace {
+    [ExportLaunchDebugTarget(
+        ProviderType,
+        new[] { PythonConstants.FileExtension, PythonConstants.WindowsFileExtension }
+    )]
+    class PythonDebugLaunchProvider : ILaunchDebugTargetProvider {
+        private const string ProviderType = "F2B8B667-3D13-4E51-B067-00C188D0EB7E";
+
+        public const string LaunchTypeName = "Python";
+
+        // Set by the workspace, not by our users
+        private const string ScriptNameKey = "target";
+
+        public const string InterpreterKey = "interpreter";
+        public const string InterpreterArgumentsKey = "interpreterArguments";
+        public const string ScriptArgumentsKey = "scriptArguments";
+        public const string WorkingDirectoryKey = "workingDirectory";
+        public const string NativeDebuggingKey = "nativeDebug";
+        public const string WebBrowserUrlKey = "webBrowserUrl";
+
+        public const string JsonSchema = @"{
+  ""definitions"": {
+    ""python"": {
+      ""type"": ""object"",
+      ""properties"": {
+        ""type"": {""type"": ""string"", ""enum"": [ ""python"" ]},
+        ""interpreter"": { ""type"": ""string"" },
+        ""interpreterArguments"": { ""type"": ""string"" },
+        ""scriptArgument"": { ""type"": ""string"" },
+        ""workingDirectory"": { ""type"": ""string"" },
+        ""nativeDebug"": { ""type"": ""boolean"" },
+        ""webBrowserUrl"": { ""type"": ""string"" }
+      }
+    },
+    ""pythonFile"": {
+      ""allOf"": [
+        { ""$ref"": ""#/definitions/default"" },
+        { ""$ref"": ""#/definitions/python"" }
+      ]
+    }
+  },
+    ""defaults"": {
+        "".py"": { ""$ref"": ""#/definitions/python"" },
+        "".pyw"": { ""$ref"": ""#/definitions/python"" }
+    },
+    ""configuration"": ""#/definitions/pythonFile""
+}";
+
+        public void LaunchDebugTarget(IServiceProvider serviceProvider, DebugLaunchActionContext debugLaunchActionContext) {
+            var registry = serviceProvider.GetComponentModel().GetService<IInterpreterRegistryService>();
+
+            var settings = debugLaunchActionContext.LaunchConfiguration;
+            var path = settings.GetValue(InterpreterKey, string.Empty);
+            InterpreterConfiguration config = null;
+
+            if (!string.IsNullOrEmpty(path)) {
+                if (PathUtils.IsValidPath(path) && !Path.IsPathRooted(path)) {
+                    // TODO: Find location of launch.json
+                }
+
+                if (File.Exists(path)) {
+                    config = registry.Configurations.FirstOrDefault(c => c.InterpreterPath.Equals(path, StringComparison.OrdinalIgnoreCase)) ??
+                        new InterpreterConfiguration("Custom", path, PathUtils.GetParent(path), path);
+                } else {
+                    config = registry.FindConfiguration(path);
+                }
+            } else {
+                var service = serviceProvider.GetComponentModel().GetService<IInterpreterOptionsService>();
+                path = service.DefaultInterpreter.Configuration.InterpreterPath;
+            }
+
+            if (!File.Exists(path)) {
+                throw new InvalidOperationException(Strings.DebugLaunchInterpreterMissing_Path.FormatUI(path));
+            }
+
+            IProjectLauncher launcher = null;
+            var launchConfig = new LaunchConfiguration(config) {
+                InterpreterPath = config == null ? path : null,
+                InterpreterArguments = settings.GetValue<string>(InterpreterArgumentsKey),
+                ScriptName = settings.GetValue<string>(ScriptNameKey),
+                ScriptArguments = settings.GetValue<string>(ScriptArgumentsKey),
+                WorkingDirectory = settings.GetValue<string>(WorkingDirectoryKey),
+                // TODO: Support search paths
+                SearchPaths = null,
+                // TODO: Support env variables
+                Environment = null,
+            };
+            launchConfig.LaunchOptions[PythonConstants.EnableNativeCodeDebugging] = settings.GetValue(NativeDebuggingKey, false).ToString();
+
+
+            var browserUrl = settings.GetValue<string>(WebBrowserUrlKey);
+            if (!string.IsNullOrEmpty(browserUrl)) {
+                launchConfig.LaunchOptions[PythonConstants.WebBrowserUrlSetting] = browserUrl;
+                launcher = new PythonWebLauncher(serviceProvider, launchConfig, launchConfig, launchConfig);
+            }
+
+            (launcher ?? new DefaultPythonLauncher(serviceProvider, launchConfig)).LaunchProject(true);
+        }
+
+        public bool SupportsContext(string filePath) {
+            throw new NotImplementedException();
+        }
+    }
+}
