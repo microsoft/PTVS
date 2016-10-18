d302b4c6f1ab756a05a9a30e169568e320253dfc
Steve Dower <steve.dower@microsoft.com>
2016-10-18 14:59:27 -0700

    Fixes #1732: Infinite DB refresh loop Checkes whether DB is actually generating when the flag is set and resets the state.

--- Python/Product/Analysis/Interpreter/PythonInterpreterFactoryWithDatabase.cs
+++ Python/Product/Analysis/Interpreter/PythonInterpreterFactoryWithDatabase.cs
@@ -218,6 +218,10 @@ namespace Microsoft.PythonTools.Interpreter {
                 onExit?.Invoke(PythonTypeDatabase.NotSupportedExitCode);
                 return;
             }
+            if (IsGenerating) {
+                onExit?.Invoke(PythonTypeDatabase.AlreadyGeneratingExitCode);
+                return;
+            }
 
             var req = new PythonTypeDatabaseCreationRequest {
                 Factory = this,
@@ -346,6 +350,16 @@ namespace Microsoft.PythonTools.Interpreter {
                 _isCurrentException = null;
                 OnIsCurrentChanged();
                 return;
+            }
+            if (IsGenerating) {
+                if (PythonTypeDatabase.IsDatabaseRegenerating(DatabasePath)) {
+                    _isValid = false;
+                    _missingModules = null;
+                    _isCurrentException = null;
+                    return;
+                }
+                var generating = Interlocked.Exchange(ref _generating, null);
+                generating?.Dispose();
             }
 
             try {
