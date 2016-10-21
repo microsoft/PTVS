aeab7b641f69e593196989d0c56ae72af33f6e95
Hugues Valois <huvalo@microsoft.com>
2016-10-21 14:47:57 -0700

    Handle errors that occur when the unit test executor is loading the tests, prior to executing them.

---
+++ Python/Tests/TestData/TestAdapterTests/LoadErrorTestImportError.py
@@ -0,0 +1,9 @@
+import unittest
+import foooo
+
+class ImportErrorTests(unittest.TestCase):
+    def test_import_error(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Python/Tests/TestData/TestAdapterTests/LoadErrorTestNoError.py
@@ -0,0 +1,8 @@
+import unittest
+
+class NoErrorTests(unittest.TestCase):
+    def test_no_error(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
