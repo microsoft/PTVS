7f64a400d4d8446eea82f9ea042dd1e431806525
Steve Dower <steve.dower@microsoft.com>
2016-11-29 13:12:10 -0800

    Fixes #1886 Remove StaticUriPattern from project templates Adds a message to the Web project property page. Fixes Django targets.

--- Python/Product/Django/Project/DjangoPropertyPageControl.Designer.cs
+++ Python/Product/Django/Project/DjangoPropertyPageControl.Designer.cs
@@ -26,33 +26,22 @@ namespace Microsoft.PythonTools.Django.Project {
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
             System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-            this._djangoGroup = new System.Windows.Forms.GroupBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DjangoPropertyPageControl));
             this._settingsModuleLabel = new System.Windows.Forms.Label();
             this._settingsModule = new System.Windows.Forms.TextBox();
             this._staticUriLabel = new System.Windows.Forms.Label();
             this._staticUri = new System.Windows.Forms.TextBox();
+            this._djangoGroup = new System.Windows.Forms.GroupBox();
             this._toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this._errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this._deprecatedLabel = new System.Windows.Forms.Label();
             tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this._djangoGroup.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
             tableLayoutPanel1.SuspendLayout();
+            this._djangoGroup.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._errorProvider)).BeginInit();
             this.SuspendLayout();
-            // 
-            // _djangoGroup
-            // 
-            this._djangoGroup.AutoSize = true;
-            this._djangoGroup.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this._djangoGroup.Controls.Add(tableLayoutPanel2);
-            this._djangoGroup.Dock = System.Windows.Forms.DockStyle.Fill;
-            this._djangoGroup.Location = new System.Drawing.Point(6, 8);
-            this._djangoGroup.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
-            this._djangoGroup.Name = "_djangoGroup";
-            this._djangoGroup.Padding = new System.Windows.Forms.Padding(6, 8, 6, 8);
-            this._djangoGroup.Size = new System.Drawing.Size(423, 81);
-            this._djangoGroup.TabIndex = 0;
-            this._djangoGroup.TabStop = false;
-            this._djangoGroup.Text = "Django";
             // 
             // tableLayoutPanel2
             // 
@@ -77,7 +66,7 @@ namespace Microsoft.PythonTools.Django.Project {
             tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
             tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
             tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            tableLayoutPanel2.Size = new System.Drawing.Size(411, 52);
+            tableLayoutPanel2.Size = new System.Drawing.Size(612, 52);
             tableLayoutPanel2.TabIndex = 0;
             // 
             // _settingsModuleLabel
@@ -99,7 +88,7 @@ namespace Microsoft.PythonTools.Django.Project {
             this._settingsModule.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
             this._settingsModule.MinimumSize = new System.Drawing.Size(50, 4);
             this._settingsModule.Name = "_settingsModule";
-            this._settingsModule.Size = new System.Drawing.Size(291, 20);
+            this._settingsModule.Size = new System.Drawing.Size(492, 20);
             this._settingsModule.TabIndex = 1;
             this._settingsModule.TextChanged += new System.EventHandler(this.Setting_TextChanged);
             // 
@@ -122,7 +111,7 @@ namespace Microsoft.PythonTools.Django.Project {
             this._staticUri.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
             this._staticUri.MinimumSize = new System.Drawing.Size(50, 4);
             this._staticUri.Name = "_staticUri";
-            this._staticUri.Size = new System.Drawing.Size(291, 20);
+            this._staticUri.Size = new System.Drawing.Size(492, 20);
             this._staticUri.TabIndex = 3;
             this._staticUri.TextChanged += new System.EventHandler(this.Setting_TextChanged);
             // 
@@ -133,15 +122,51 @@ namespace Microsoft.PythonTools.Django.Project {
             tableLayoutPanel1.ColumnCount = 1;
             tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             tableLayoutPanel1.Controls.Add(this._djangoGroup, 0, 0);
+            tableLayoutPanel1.Controls.Add(this._deprecatedLabel, 0, 1);
             tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            tableLayoutPanel1.Size = new System.Drawing.Size(435, 117);
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(636, 142);
             tableLayoutPanel1.TabIndex = 0;
+            // 
+            // _djangoGroup
+            // 
+            this._djangoGroup.AutoSize = true;
+            this._djangoGroup.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this._djangoGroup.Controls.Add(tableLayoutPanel2);
+            this._djangoGroup.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._djangoGroup.Location = new System.Drawing.Point(6, 8);
+            this._djangoGroup.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this._djangoGroup.Name = "_djangoGroup";
+            this._djangoGroup.Padding = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this._djangoGroup.Size = new System.Drawing.Size(624, 81);
+            this._djangoGroup.TabIndex = 0;
+            this._djangoGroup.TabStop = false;
+            this._djangoGroup.Text = "Django";
+            // 
+            // _errorProvider
+            // 
+            this._errorProvider.ContainerControl = this;
+            this._errorProvider.Icon = ((System.Drawing.Icon)(resources.GetObject("_errorProvider.Icon")));
+            // 
+            // _deprecatedLabel
+            // 
+            this._deprecatedLabel.AutoSize = true;
+            this._errorProvider.SetError(this._deprecatedLabel, "Deprecated");
+            this._errorProvider.SetIconAlignment(this._deprecatedLabel, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
+            this._deprecatedLabel.Location = new System.Drawing.Point(24, 103);
+            this._deprecatedLabel.Margin = new System.Windows.Forms.Padding(24, 6, 3, 6);
+            this._deprecatedLabel.Name = "_deprecatedLabel";
+            this._deprecatedLabel.Size = new System.Drawing.Size(609, 13);
+            this._deprecatedLabel.TabIndex = 1;
+            this._deprecatedLabel.Text = "These options are no longer used when publishing web sites. To configure publish " +
+    "settings, add a web.config file to your project.";
             // 
             // DjangoPropertyPageControl
             // 
@@ -152,13 +177,14 @@ namespace Microsoft.PythonTools.Django.Project {
             this.Controls.Add(tableLayoutPanel1);
             this.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.Name = "DjangoPropertyPageControl";
-            this.Size = new System.Drawing.Size(435, 117);
-            this._djangoGroup.ResumeLayout(false);
-            this._djangoGroup.PerformLayout();
+            this.Size = new System.Drawing.Size(636, 142);
             tableLayoutPanel2.ResumeLayout(false);
             tableLayoutPanel2.PerformLayout();
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
+            this._djangoGroup.ResumeLayout(false);
+            this._djangoGroup.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._errorProvider)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,5 +198,7 @@ namespace Microsoft.PythonTools.Django.Project {
         private System.Windows.Forms.TextBox _settingsModule;
         private System.Windows.Forms.Label _staticUriLabel;
         private System.Windows.Forms.TextBox _staticUri;
+        private System.Windows.Forms.ErrorProvider _errorProvider;
+        private System.Windows.Forms.Label _deprecatedLabel;
     }
 }
