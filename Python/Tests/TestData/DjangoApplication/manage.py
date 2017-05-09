31c6a7613d4d55f01504dab4c61d3661f4140831
Hugues Valois <huvalo@microsoft.com>
2017-05-09 16:50:11 -0700

    Fix DjangoCollectStaticFilesCommand test data.

--- Python/Tests/TestData/DjangoApplication/manage.py
+++ Python/Tests/TestData/DjangoApplication/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoApplication1.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoApplication.settings")
 
     from django.core.management import execute_from_command_line
 
