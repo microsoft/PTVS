dcf11d2d26519cdea10813530d2bde85f8fe8180
Hugues Valois <huvalo@microsoft.com>
2017-06-22 11:21:30 -0700

    Use a dict instead of Context instance in DjangoDebuggerTests test project, to avoid a TypeError in latest version of Django.

--- Python/Tests/TestData/DjangoProject/Oar/views.py
+++ Python/Tests/TestData/DjangoProject/Oar/views.py
@@ -11,29 +11,29 @@ def main(request):
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     t = loader.get_template('polls/index.html')
-    c = Context({
+    c = {
         'latest_poll_list': latest_poll_list,
-    })
+    }
     return HttpResponse(t.render(c))
 
 def loop(request):
     t = loader.get_template('polls/loop.html')
-    c = Context({
+    c = {
         'colors': ['red', 'blue', 'green']
-    })
+    }
     return HttpResponse(t.render(c))
 
 def loop_nobom(request):
     t = loader.get_template('polls/loop_nobom.html')
-    c = Context({
+    c = {
         'colors': ['red', 'blue', 'green']
-    })
+    }
     return HttpResponse(t.render(c))
 
 def loop2(request):
     t = loader.get_template('polls/loop2.html')
-    c = Context({
+    c = {
         'colors': ['red', 'blue', 'green']
-    })
+    }
     return HttpResponse(t.render(c))
 
