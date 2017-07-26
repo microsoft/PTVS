2c27ffbfb31b551b23880129bb2864ad614572df
Steve Dower <steve.dower@microsoft.com>
2017-07-26 13:56:20 -0700

    Enables environment configuration Enables Ctrl+F5

--- Python/Product/VCDebugLauncher/PythonDebugLaunchProvider.cs
+++ Python/Product/VCDebugLauncher/PythonDebugLaunchProvider.cs
@@ -15,8 +15,11 @@
 // permissions and limitations under the License.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.Debugger;
 using Microsoft.VisualStudio.ProjectSystem;
@@ -37,10 +40,6 @@ namespace Microsoft.PythonTools.Debugger.VCLauncher {
 
 
         private async Task<DebugLaunchSettings> CreateLaunchSettingsAsync(DebugLaunchOptions options) {
-            if (options == DebugLaunchOptions.NoDebug) {
-                return null;
-            }
-
             var settings = new DebugLaunchSettings(options) {
                 LaunchOperation = DebugLaunchOperation.CreateProcess,
                 LaunchDebugEngineGuid = DkmEngineId.NativeEng,
@@ -54,7 +53,55 @@ namespace Microsoft.PythonTools.Debugger.VCLauncher {
             settings.Arguments = await props.LocalDebuggerCommandArguments.GetEvaluatedValueAtEndAsync().ConfigureAwait(false);
             settings.CurrentDirectory = await props.LocalDebuggerWorkingDirectory.GetEvaluatedValueAtEndAsync().ConfigureAwait(false);
 
+            var envString = await props.LocalDebuggerEnvironment.GetEvaluatedValueAtEndAsync().ConfigureAwait(false);
+
+            if (!string.IsNullOrEmpty(envString)) {
+                var mergeEnv = await props.LocalDebuggerMergeEnvironment.GetEvaluatedValueAtEndAsync().ConfigureAwait(false);
+                if ("true".Equals(mergeEnv ?? "", StringComparison.OrdinalIgnoreCase)) {
+                    FillFromCurrentEnvironment(settings.Environment);
+                }
+                ParseEnvironment(settings.Environment, envString);
+            }
+
             return settings;
+        }
+
+        private static void FillFromCurrentEnvironment(IDictionary<string, string> env) {
+            foreach (DictionaryEntry kv in Environment.GetEnvironmentVariables()) {
+                var k = kv.Key as string;
+                var v = kv.Value as string;
+                if (string.IsNullOrEmpty(k) || string.IsNullOrEmpty(v)) {
+                    continue;
+                }
+                env[k] = v;
+            }
+        }
+
+        private static void ParseEnvironment(IDictionary<string, string> env, string envString) {
+            foreach (var kv in envString.Split('\n')) {
+                int i = kv.IndexOf('=');
+                if (i < 1 || i >= kv.Length - 1) {
+                    continue;
+                }
+                var k = kv.Substring(0, i);
+                var v = kv.Substring(i + 1);
+                if (string.IsNullOrEmpty(k) || string.IsNullOrEmpty(v)) {
+                    continue;
+                }
+
+                v = Regex.Replace(v, "%([\\w_]+)%", m => {
+                    if (!m.Groups[1].Success) {
+                        return m.Value;
+                    }
+                    string newV;
+                    if (env.TryGetValue(m.Groups[1].Value, out newV)) {
+                        return newV;
+                    }
+                    return m.Value;
+                });
+
+                env[k] = v;
+            }
         }
 
         public async override Task<bool> CanLaunchAsync(DebugLaunchOptions launchOptions) {
