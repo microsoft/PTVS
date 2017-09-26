8d20f6b14e6ed3b25b229effe4a8ad4fee170a73
Steve Dower <steve.dower@microsoft.com>
2017-09-26 14:11:33 -0700

    Fixes #3067 "Reports" in profiling is not localized Moves string literal into resource.

--- Python/Product/Profiling/Strings.Designer.cs
+++ Python/Product/Profiling/Strings.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.PythonTools.Profiling {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Strings {
@@ -475,6 +475,15 @@ namespace Microsoft.PythonTools.Profiling {
         public static string PythonFilesFilter {
             get {
                 return ResourceManager.GetString("PythonFilesFilter", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Reports.
+        /// </summary>
+        public static string Reports {
+            get {
+                return ResourceManager.GetString("Reports", resourceCulture);
             }
         }
         
