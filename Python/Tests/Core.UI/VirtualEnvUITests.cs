d3508471a01079fdc52c182f633cb0f87b1ef536
Hugues Valois <huvalo@microsoft.com>
2017-10-19 13:57:20 -0700

    Add Python 3.7 to the list of interpreters for virtual environment UI tests.

--- Python/Tests/Core.UI/VirtualEnvUITests.cs
+++ Python/Tests/Core.UI/VirtualEnvUITests.cs
@@ -52,6 +52,7 @@ namespace PythonToolsUITests {
 
         static DefaultInterpreterSetter InitPython3(PythonVisualStudioApp app) {
             return app.SelectDefaultInterpreter(
+                PythonPaths.Python37 ?? PythonPaths.Python37_x64 ??
                 PythonPaths.Python36 ?? PythonPaths.Python36_x64 ??
                 PythonPaths.Python35 ?? PythonPaths.Python35_x64 ??
                 PythonPaths.Python34 ?? PythonPaths.Python34_x64 ??
