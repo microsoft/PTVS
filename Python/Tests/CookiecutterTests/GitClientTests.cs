08fed996c87c3c66643b5e69df058f483e7281de
Hugues Valois <huvalo@microsoft.com>
2017-10-23 14:16:57 -0700

    Add test for cloning non-existent repo.

--- Python/Tests/CookiecutterTests/GitClientTests.cs
+++ Python/Tests/CookiecutterTests/GitClientTests.cs
@@ -42,5 +42,22 @@ namespace CookiecutterTests {
             // Make sure failed clone is cleaned up
             Assert.AreEqual(0, Directory.GetDirectories(outputParentFolder).Length);
         }
+
+        [TestMethod]
+        public async Task CloneNonExistentRepo() {
+            var client = GitClientProvider.Create(null, null);
+
+            var outputParentFolder = TestData.GetTempPath();
+
+            try {
+                await client.CloneAsync("https://github.com/Microsoft/non-existent-repo", outputParentFolder);
+                Assert.Fail($"Failed to generate exception when cloning non-existent repository.");
+            } catch (ProcessException ex) {
+                Assert.AreNotEqual(0, ex.Result.StandardErrorLines.Length);
+            }
+
+            // Make sure failed clone is cleaned up
+            Assert.AreEqual(0, Directory.GetDirectories(outputParentFolder).Length);
+        }
     }
 }
