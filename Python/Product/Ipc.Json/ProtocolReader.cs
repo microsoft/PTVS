b271f880e0db8af60edb791f992191826d8fda36
Steve Dower <steve.dower@microsoft.com>
2018-01-08 21:01:23 +1100

    Another attempt at dealing with the UTF-8 BOM in headers.

--- Python/Product/Ipc.Json/ProtocolReader.cs
+++ Python/Product/Ipc.Json/ProtocolReader.cs
@@ -57,7 +57,7 @@ namespace Microsoft.PythonTools.Ipc.Json {
                 // Should not have UTF-8 headers, but .NET's Process class
                 // doesn't give us that much control over stdin encoding,
                 // often writing the BOM before we can bypass it.
-                return Encoding.UTF8.GetString(line);
+                line = line.Skip(3).ToArray();
             }
             return Encoding.ASCII.GetString(line);
         }
