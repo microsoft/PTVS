95b2c8ad52f6ea4d3c896f8dc0292fdc4022eda6
Steve Dower <steve.dower@microsoft.com>
2018-01-23 08:55:57 +1100

    Fixes null reference for uncalled function.

--- Python/Product/Analysis/Values/FunctionInfo.cs
+++ Python/Product/Analysis/Values/FunctionInfo.cs
@@ -20,6 +20,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using Microsoft.PythonTools.Analysis.Analyzer;
+using Microsoft.PythonTools.Analysis.Infrastructure;
 using Microsoft.PythonTools.Interpreter;
 using Microsoft.PythonTools.Parsing;
 using Microsoft.PythonTools.Parsing.Ast;
@@ -530,7 +531,7 @@ namespace Microsoft.PythonTools.Analysis.Values {
                         vars = vars.AsStrongerUnion();
                     }
                 }
-                foreach (var unit in _allCalls.Values) {
+                foreach (var unit in (_allCalls?.Values).MaybeEnumerate()) {
                     if (unit.Scope.TryGetVariable(curParam.Name, out vd)) {
                         vars = vars.Union(vd.TypesNoCopy);
                         if (vars.Count > limit) {
