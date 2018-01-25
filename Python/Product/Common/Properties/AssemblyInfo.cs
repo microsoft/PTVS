636353bcf8820e4b55ca5695c8c232e7fb55578f
Steve Dower <steve.dower@microsoft.com>
2018-01-25 12:22:58 +1100

    Removes VS version from end of assembly version

--- Python/Product/Common/Properties/AssemblyInfo.cs
+++ Python/Product/Common/Properties/AssemblyInfo.cs
@@ -27,10 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-// The common DLL doesn't version with Visual Studio.  This allows running it out of proc and having
-// extensions which support all installed versions.  When we load extensions for analysis we load compatible
-// versions, and load only the latest compatible version available.
-[assembly: AssemblyVersion(AssemblyVersionInfo.StableVersionPrefix + ".0")]
+[assembly: AssemblyVersion(AssemblyVersionInfo.StableVersion)]
 
 [assembly: InternalsVisibleTo("Microsoft.PythonTools.Analysis, PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1fa57c4aed9f0a32e84aa0faefd0de9e8fd6aec8f87fb03766c834c99921eb23be79ad9d5dcc1dd9ad236132102900b723cf980957fc4e177108fc607774f29e8320e92ea05ece4e821c0a5efe8f1645c4c0c93c1ab99285d622caa652c1dfad63d745d6f2de5f17e5eaf0fc4963d261c8a12436518206dc093344d5ad293")]
 [assembly: InternalsVisibleTo("Microsoft.PythonTools.Analyzer, PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1fa57c4aed9f0a32e84aa0faefd0de9e8fd6aec8f87fb03766c834c99921eb23be79ad9d5dcc1dd9ad236132102900b723cf980957fc4e177108fc607774f29e8320e92ea05ece4e821c0a5efe8f1645c4c0c93c1ab99285d622caa652c1dfad63d745d6f2de5f17e5eaf0fc4963d261c8a12436518206dc093344d5ad293")]
