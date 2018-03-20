7470a2a3dc481e10b7f891a90d14c1baa8ee7bd3
R. G. Esteves <rgesteves@gmail.com>
2018-03-20 12:02:07 -0700

    Moved all code to shared vscode build

--- Python/Product/ExternalProfilerDriver/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("PTVS External Profiler driver")]
-[assembly: AssemblyDescription("Provides a profiler 'hub' that allows invocation of various Python profilers.")]
-
