f8eed89295eb3bdbce27632898c36d83d92754fd
Alexander Sher <alsher@microsoft.com>
2018-04-12 21:22:06 -0700

    - Fix CodeFormatterTests so that test waits until analyzer finish parsing - Switch ExtractMethodTests to real editor (100% pass)

---
+++ Python/Tests/Utilities.Python/EventTaskSources.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUtilities.Python {
+    internal static class EventTaskSources {
+        public static class VsProjectAnalyzer {
+            public static readonly EventTaskSource<Microsoft.PythonTools.Intellisense.VsProjectAnalyzer> AnalysisStarted =
+                new EventTaskSource<Microsoft.PythonTools.Intellisense.VsProjectAnalyzer>(
+                    (o, e) => o.AnalysisStarted += e,
+                    (o, e) => o.AnalysisStarted -= e);
+        }
+    }
+}
