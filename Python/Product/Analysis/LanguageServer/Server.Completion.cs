48c2e924f510a20da8aabdd64c353d1b83692f53
MikhailArkhipov <mikhaila@microsoft.com>
2018-04-27 15:04:07 -0700

    Merge issues

--- Python/Product/Analysis/LanguageServer/Server.Completion.cs
+++ Python/Product/Analysis/LanguageServer/Server.Completion.cs
@@ -14,11 +14,10 @@
 // See the Apache Version 2.0 License for specific language governing
 // permissions and limitations under the License.
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.PythonTools.Analysis.Infrastructure;
 using Microsoft.PythonTools.Analysis.Pythia;
-using Microsoft.PythonTools.Interpreter;
 using Microsoft.PythonTools.Parsing;
 
 namespace Microsoft.PythonTools.Analysis.LanguageServer {
