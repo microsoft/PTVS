561ed6cdafac5307192aba1aadf4ec9630de77f3
MikhailArkhipov <mikhaila@microsoft.com>
2018-05-08 15:50:03 -0700

    Put Pythia back

---
+++ Python/Product/Analysis/Pythia/UsageDataModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Microsoft.PythonTools.Analysis.Pythia {
+    public class UsageDataModel {
+        [JsonProperty("Repo")]
+        public string Repo;
+
+        [JsonProperty("Project")]
+        public string Project;
+
+        [JsonProperty("Document")]
+        public string Document;
+
+        [JsonProperty("References")]
+        public Dictionary<string, Dictionary<string, Invocations>> References;
+    }
+
+    public class Invocations {
+        //public string MethodName { get; set; }
+
+        [JsonProperty("spanStart")]
+        public List<int> SpanStart { get; set; }
+
+        [JsonProperty("isInConditional")]
+        public List<int> IsInConditional { get; set; }
+
+        [JsonProperty("isInLoop")]
+        public List<int> IsInLoop { get; set; }
+    }
+}
