11d20261389ce40cf4e3a192542057e95eafe4de
MikhailArkhipov <mikhaila@microsoft.com>
2018-06-20 16:15:14 -0700

    Better support from import in tooltips

--- Python/Product/Analysis/Intellisense/ImportedModuleNameWalker.cs
+++ Python/Product/Analysis/Intellisense/ImportedModuleNameWalker.cs
@@ -25,7 +25,7 @@ namespace Microsoft.PythonTools.Intellisense {
         private readonly string _importingFromModuleName;
         private readonly string _importingFromFilePath;
 
-        public ImportedModuleNameWalker(IPythonProjectEntry entry, int location) : 
+        public ImportedModuleNameWalker(IPythonProjectEntry entry, int location) :
             this(entry.ModuleName, entry.FilePath, location) {
         }
         public ImportedModuleNameWalker(string importingFromModuleName, string importingFromFilePath, int location) : base(location) {
@@ -34,23 +34,39 @@ namespace Microsoft.PythonTools.Intellisense {
         }
 
         public IEnumerable<string> ImportedModules { get; private set; } = Enumerable.Empty<string>();
+        public IEnumerable<string> ImportedMembers { get; private set; } = Enumerable.Empty<string>();
+        public string ImportedType { get; private set; }
 
         public override bool Walk(FromImportStatement node) {
             if (node.StartIndex <= Location && Location <= node.EndIndex) {
                 // Determine if location is over imported parts such as 
                 // over 'a' in 'from . import a, b, c' or over 'x' in 'from a import x'
                 // and store module names and imported parts
-                ImportedModules = ModuleResolver.ResolvePotentialModuleNames(_importingFromModuleName, _importingFromFilePath, node.Root.MakeString(), node.ForceAbsolute);
+                var modName = node.Root.MakeString();
+                var nameNode = node.Names.MaybeEnumerate().Where(n => n.StartIndex <= Location && Location <= n.EndIndex).FirstOrDefault();
+                if (nameNode != null) {
+                    // Over name, see if we have 'as' and fetch type from there
+                    if (node.AsNames != null) {
+                        var index = node.Names.IndexOf(nameNode);
+                        if (index < node.AsNames.Count) {
+                            ImportedType = node.AsNames[index]?.Name;
+                            return false;
+                        }
+                    }
+                    // See if we can resolve relative names
+                    var candidates = ModuleResolver.ResolvePotentialModuleNames(_importingFromModuleName, _importingFromFilePath, modName, node.ForceAbsolute);
+                    ImportedMembers = candidates.Select(c => "{0}.{1}".FormatInvariant(c, nameNode.Name));
+                } else {
+                    ImportedModules = new[] { modName };
+                }
             }
             return false;
         }
 
         public override bool Walk(ImportStatement node) {
-            foreach (var n in node.Names.MaybeEnumerate()) {
-                if (n.StartIndex <= Location && Location <= n.EndIndex) {
-                    ImportedModules = new[] { n.MakeString() };
-                    break;
-                }
+            if (node.StartIndex <= Location && Location <= node.EndIndex) {
+                var nameNode = node.Names.MaybeEnumerate().Where(n => n.StartIndex <= Location && Location <= n.EndIndex).FirstOrDefault();
+                ImportedModules = nameNode != null ? new[] { nameNode.MakeString() } : Enumerable.Empty<string>();
             }
             return false;
         }
--- Python/Product/Analysis/LanguageServer/Server.Hover.cs
+++ Python/Product/Analysis/LanguageServer/Server.Hover.cs
@@ -14,6 +14,7 @@
 // See the Apache Version 2.0 License for specific language governing
 // permissions and limitations under the License.
 
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -42,9 +43,62 @@ namespace Microsoft.PythonTools.Analysis.LanguageServer {
 
             tree = GetParseTree(entry, uri, CancellationToken, out var version) ?? tree;
 
+            Expression expr;
+            SourceSpan? exprSpan;
+            Analyzer.InterpreterScope scope = null;
+
+            var finder = new ExpressionFinder(tree, GetExpressionOptions.Hover);
+            expr = finder.GetExpression(@params.position) as Expression;
+            exprSpan = expr?.GetSpan(tree);
+
+            if (expr == null) {
+                TraceMessage($"No hover info found in {uri} at {@params.position}");
+                return Task.FromResult(EmptyHover);
+            }
+
+            TraceMessage($"Getting hover for {expr.ToCodeString(tree, CodeFormattingOptions.Traditional)}");
+
+            // First try values from expression. This works for 
+            // import statements most of the time as well.
             var index = tree.LocationToIndex(@params.position);
             var w = new ImportedModuleNameWalker(entry, index);
             tree.Walk(w);
+
+            IEnumerable<AnalysisValue> values = null;
+            // Perhaps it is an assigned member like 'p' in 'from os import path as p'
+            if (!string.IsNullOrEmpty(w.ImportedType)) {
+                values = analysis.GetValues(w.ImportedType, @params.position).ToList();
+            } else {
+                // Perhaps it is a member like 'path' in 'from os import path'
+                w.ImportedMembers.FirstOrDefault(m => {
+                    values = analysis.GetValues(m, @params.position).ToList();
+                    return values.Any();
+                });
+            }
+
+            values = values ?? analysis.GetValues(expr, @params.position, scope).ToList();
+            if (values.Any()) {
+                string originalExpr;
+                if (expr is ConstantExpression || expr is ErrorExpression) {
+                    originalExpr = null;
+                } else {
+                    originalExpr = @params._expr?.Trim();
+                    if (string.IsNullOrEmpty(originalExpr)) {
+                        originalExpr = expr.ToCodeString(tree, CodeFormattingOptions.Traditional);
+                    }
+                }
+
+                var names = values.Select(GetFullTypeName).Where(n => !string.IsNullOrEmpty(n)).Distinct().ToArray();
+                var res = new Hover {
+                    contents = GetMarkupContent(
+                        _displayTextBuilder.GetDocumentation(values, originalExpr),
+                        _clientCaps.textDocument?.hover?.contentFormat),
+                    range = exprSpan,
+                    _version = version?.Version,
+                    _typeNames = names
+                };
+                return Task.FromResult(res);
+            }
 
             if (w.ImportedModules.Any()) {
                 var sb = new StringBuilder();
@@ -62,43 +116,7 @@ namespace Microsoft.PythonTools.Analysis.LanguageServer {
                 }
             }
 
-            Expression expr;
-            SourceSpan? exprSpan;
-            Analyzer.InterpreterScope scope = null;
-
-            var finder = new ExpressionFinder(tree, GetExpressionOptions.Hover);
-            expr = finder.GetExpression(@params.position) as Expression;
-            exprSpan = expr?.GetSpan(tree);
-
-            if (expr == null) {
-                TraceMessage($"No hover info found in {uri} at {@params.position}");
-                return Task.FromResult(EmptyHover);
-            }
-
-            TraceMessage($"Getting hover for {expr.ToCodeString(tree, CodeFormattingOptions.Traditional)}");
-            var values = analysis.GetValues(expr, @params.position, scope).ToList();
-
-            string originalExpr;
-            if (expr is ConstantExpression || expr is ErrorExpression) {
-                originalExpr = null;
-            } else {
-                originalExpr = @params._expr?.Trim();
-                if (string.IsNullOrEmpty(originalExpr)) {
-                    originalExpr = expr.ToCodeString(tree, CodeFormattingOptions.Traditional);
-                }
-            }
-
-            var names = values.Select(GetFullTypeName).Where(n => !string.IsNullOrEmpty(n)).Distinct().ToArray();
-
-            var res = new Hover {
-                contents = GetMarkupContent(
-                    _displayTextBuilder.GetDocumentation(values, originalExpr),
-                    _clientCaps.textDocument?.hover?.contentFormat),
-                range = exprSpan,
-                _version = version?.Version,
-                _typeNames = names
-            };
-            return Task.FromResult(res);
+            return Task.FromResult(EmptyHover);
         }
 
         private static string GetFullTypeName(AnalysisValue value) {
