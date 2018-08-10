7b004c723a3817af5f5d242af56b414b74d7aa4d
MikhailArkhipov <mikhaila@microsoft.com>
2018-08-10 13:15:59 -0700

    PR feedback

--- Python/Product/Analysis/Values/BoundBuiltinMethodInfo.cs
+++ Python/Product/Analysis/Values/BoundBuiltinMethodInfo.cs
@@ -53,7 +53,7 @@ namespace Microsoft.PythonTools.Analysis.Values {
         public override IAnalysisSet Call(Node node, AnalysisUnit unit, IAnalysisSet[] args, NameExpression[] keywordArgNames) {
             // Check if method returns self
             var returnType = _method.ReturnTypes.GetInstanceType();
-            if (args.Length > 0 && returnType.FirstOrDefault() is BuiltinInstanceInfo biif && biif.PythonType == _method.Function?.DeclaringType) {
+            if (args.Length > 0 && returnType.Split(v => v is BuiltinInstanceInfo biif && biif.PythonType == _method.Function?.DeclaringType, out _, out _)) {
                 return args[0]; //  Return actual self (i.e. derived class)
             }
             return returnType;
