dac165c373d3a406a0a07ffe274bb0c69de57897
Raymon Gulati <raymon.gulati@yahoo.com>
2019-03-01 14:17:50 -0800

    Remove GUID attribute from assembly information

--- Common/Tools/BuildTasks/Properties/AssemblyInfo.cs
+++ Common/Tools/BuildTasks/Properties/AssemblyInfo.cs
@@ -22,8 +22,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("861b9a5e-f07e-436b-a737-24072a2e6b55")]
-
-
