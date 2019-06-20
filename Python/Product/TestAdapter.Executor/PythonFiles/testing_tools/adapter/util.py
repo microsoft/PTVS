90a2f73511d6eec5f7cefdebf84bd317ea7b0417
Bill Schnurr <bschnurr@microsoft.com>
2019-06-20 14:08:40 -0700

    updating pytest adapter from VSCode. PR Fix some problems in the pytest adapter. (#5648)
    - note still have an issue with module folders

--- Python/Product/TestAdapter.Executor/PythonFiles/testing_tools/adapter/util.py
+++ Python/Product/TestAdapter.Executor/PythonFiles/testing_tools/adapter/util.py
@@ -17,17 +17,45 @@ def noop_cm():
 @contextlib.contextmanager
 def hide_stdio():
     """Swallow stdout and stderr."""
-    ignored = IgnoredIO()
+    ignored = StdioStream()
     sys.stdout = ignored
     sys.stderr = ignored
     try:
-        yield
+        yield ignored
     finally:
         sys.stdout = sys.__stdout__
         sys.stderr = sys.__stderr__
 
 
-class IgnoredIO(StringIO):
-    """A noop "file"."""
-    def write(self, msg):
-        pass
+if sys.version_info < (3,):
+    class StdioStream(StringIO):
+        def write(self, msg):
+            StringIO.write(self, msg.decode())
+else:
+    StdioStream = StringIO
+
+
+def group_attr_names(attrnames):
+    grouped = {
+            'dunder': [],
+            'private': [],
+            'constants': [],
+            'classes': [],
+            'vars': [],
+            'other': [],
+            }
+    for name in attrnames:
+        if name.startswith('__') and name.endswith('__'):
+            group = 'dunder'
+        elif name.startswith('_'):
+            group = 'private'
+        elif name.isupper():
+            group = 'constants'
+        elif name.islower():
+            group = 'vars'
+        elif name == name.capitalize():
+            group = 'classes'
+        else:
+            group = 'other'
+        grouped[group].append(name)
+    return grouped
