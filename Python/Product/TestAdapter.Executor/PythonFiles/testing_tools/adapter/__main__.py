4c712f0d8fdc668e0b1e9fbb56e9fd5c3248dee3
Bill Schnurr <bschnurr@microsoft.com>
2019-07-19 11:32:57 -0700

    removing commented out debugger code

--- Python/Product/TestAdapter.Executor/PythonFiles/testing_tools/adapter/__main__.py
+++ Python/Product/TestAdapter.Executor/PythonFiles/testing_tools/adapter/__main__.py
@@ -5,20 +5,9 @@ from __future__ import absolute_import
 
 import argparse
 import sys
-#import ptvsd
 
 from . import pytest, report, unittest
 from .errors import UnsupportedToolError, UnsupportedCommandError
-
-
-# 5678 is the default attach port in the VS Code debug configurations
-#print("Waiting for debugger attach")
-#ptvsd.enable_attach(address=('localhost', 5678), redirect_output=True)
-#ptvsd.wait_for_attach()
-#breakpoint()
-
-
-
 
 TOOLS = {
     'pytest': {
