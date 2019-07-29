1c750b090dd83045a1a5196f5543e22ea3af5c2c
Bill Schnurr <bschnurr@microsoft.com>
2019-07-29 11:22:33 -0700

    Unit test discovery assert when test in sub folder bug #5477 (#5492)
    * Unit test discovery assert when test in sub folder bug #5477
    
    -now we append the last two elements from the testId which should be classname and functionname to the module relative path.
    
    -Gathering the source file of the test proved difficult. Previous version was using the module file but for anything in a package it was returning __init__.py.
    
    Then trying getsourcefile on the test object would return the wrong file is the test was decorated.
    My solution is to call getsourcefile on the test's parent, which should be the class object, which still returns the correct filename even if the class is decorated.
    
    * adding test cases around decorators
    
    -Added an extra test that gets ignored until we fix decorator issue  #5497

--- Python/Product/TestAdapter.Executor/PythonFiles/testing_tools/adapter/unittest/_discovery.py
+++ Python/Product/TestAdapter.Executor/PythonFiles/testing_tools/adapter/unittest/_discovery.py
@@ -20,38 +20,44 @@ def discover(pytestargs=None, hidestdio=False,
         for cls in suite._tests:
             try:
                 for test in cls._tests:
-                    #Find source, lineno from TestId and add them to test object
-                    parts = test.id().split('.')
-                    error_case, error_message = None, None
+                    try:    
+                        #Find source, lineno from TestId and add them to test object
+                        parts = test.id().split('.')
+                        error_case, error_message = None, None
                     
-                    #Used loadTestsFromName(..) as an example of finding source of a function
-                    #https://github.com/python/cpython/blob/master/Lib/unittest/loader.py
+                        #Used loadTestsFromName(..) as an example of finding source of a function
+                        #https://github.com/python/cpython/blob/master/Lib/unittest/loader.py
 
-                    parts_copy = parts[:]
-                    while parts_copy:
-                        try:
-                            module_name = '.'.join(parts_copy)
-                            module = __import__(module_name)
-                            break
-                        except ImportError:
-                            next_attribute = parts_copy.pop()
+                        parts_copy = parts[:]
+                        while parts_copy:
+                            try:
+                                module_name = '.'.join(parts_copy)
+                                module = __import__(module_name)
+                                break
+                            except ImportError:
+                                next_attribute = parts_copy.pop()
                             
-                    parts = parts[1:]
-
-                    filename = inspect.getsourcefile(module)
-                    setattr(test, 'source', filename)
+                        parts = parts[1:]
                     
-                    obj = module
-                    for part in parts:
-                        try:
-                            parent, obj = obj, getattr(obj, part)
-                        except AttributeError as e:
-                            pass
+                        obj = module
+                        for part in parts:
+                            try:
+                                parent, obj = obj, getattr(obj, part)
+                            except AttributeError as e:
+                                pass
 
-                    if (isinstance(obj, types.FunctionType) or
-                        ((sys.version_info[0] < 3) and isinstance(obj, types.UnboundMethodType))):
-                        _, lineno = inspect.getsourcelines(obj)
-                        setattr(test, 'lineno', lineno)
+                        if (isinstance(obj, types.FunctionType) or
+                            ((sys.version_info[0] < 3) and isinstance(obj, types.UnboundMethodType))):
+                        
+                            #workaround for decorators on functions return the source of decorator and not the actual function
+                            #We return the finename of our parent which should be the class, works even if class also has a decorator
+                            filename = inspect.getsourcefile(parent)
+                            setattr(test, 'source', filename)
+
+                            _, lineno = inspect.getsourcelines(obj)
+                            setattr(test, 'lineno', lineno)
+                    except:
+                        pass
             except:
                 pass
     return (
