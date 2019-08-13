6dad720966eb353bf32f3b97670371f76fdbe3e1
Bill Schnurr <bschnurr@microsoft.com>
2019-08-13 15:05:30 -0700

    changing indentation

--- Python/Tests/TestAdapterTests/TestResultParserTests.cs
+++ Python/Tests/TestAdapterTests/TestResultParserTests.cs
@@ -26,21 +26,24 @@ namespace TestAdapterTests {
         public void CreatePytestId_FuncInsideClass() {
             Assert.AreEqual(
                 ".\\test2.py::Test_test2::test_A", 
-                TestResultParser.CreatePytestId("test2.py", "test2.Test_test2", "test_A"));
+                TestResultParser.CreatePytestId("test2.py", "test2.Test_test2", "test_A")
+            );
         }
 
         [TestMethod]
         public void CreatePytestId_GlobalFunc() {
             Assert.AreEqual(
                 ".\\test_sample.py::test_answer", 
-                TestResultParser.CreatePytestId("test_sample.py", "test_sample", "test_answer"));
+                TestResultParser.CreatePytestId("test_sample.py", "test_sample", "test_answer")
+            );
         }
 
         [TestMethod]
         public void CreatePytestId_ClassFuncWithRelativeFilename() {
             Assert.AreEqual(
                 ".\\package1\\packageA\\test1.py::Test_test1::test_A", 
-                TestResultParser.CreatePytestId("package1\\packageA\\test1.py", "package1.packageA.test1.Test_test1", "test_A"));
+                TestResultParser.CreatePytestId("package1\\packageA\\test1.py", "package1.packageA.test1.Test_test1", "test_A")
+            );
         }
 
         [TestMethod]
@@ -50,10 +53,13 @@ namespace TestAdapterTests {
             var pytestId = ".\\package1\\packageA\\test1.py::Test_test1::test_A";
 
             //Note: ignoring case since vsTestResult lookup ignores
-            Assert.AreEqual(string.Compare(
-               PyTestExtensions.CreateProperCasedPytestId(projectRoot+filename, projectRoot, pytestId),
-               TestResultParser.CreatePytestId(filename.ToLower(), "package1.packageA.test1.Test_test1", "test_A"),
-               ignoreCase:true), 0);
+            Assert.AreEqual(
+                string.Compare(
+                    PyTestExtensions.CreateProperCasedPytestId(projectRoot+filename, projectRoot, pytestId),
+                    TestResultParser.CreatePytestId(filename.ToLower(), "package1.packageA.test1.Test_test1", "test_A"),
+                    ignoreCase:true), 
+                0
+            );
         }
     }
 }
