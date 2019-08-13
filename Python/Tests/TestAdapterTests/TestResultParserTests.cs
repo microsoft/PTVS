3ad32a37905041168c1856fe24a049fd9ec1e170
Bill Schnurr <bschnurr@microsoft.com>
2019-08-13 13:33:06 -0700

    cleaning up new testfile

--- Python/Tests/TestAdapterTests/TestResultParserTests.cs
+++ Python/Tests/TestAdapterTests/TestResultParserTests.cs
@@ -1,57 +1,10 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.PythonTools.TestAdapter.Pytest;
 
 namespace TestAdapterTests {
-    /// <summary>
-    /// Summary description for TestResultParserTests
-    /// </summary>
+
     [TestClass]
     public class TestResultParserTests {
-        public TestResultParserTests() {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext {
-            get {
-                return testContextInstance;
-            }
-            set {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
 
         [TestMethod]
         public void CreatePytestId_FuncInsideClass() {
