8b1f91774712461b279ee63112d29a763cb2fe81
Bill Schnurr <bschnurr@microsoft.com>
2019-10-07 15:25:01 -0700

    Fix for an exception related to the collection changing while we are … (#5791)
    * Fix for an exception related to the collection changing while we are iterating.
    
    Steps to repro.
    1. Opening ptvsd in openfolder mode
    2.  install/uninstall pytest multiple times
    3. Test discovery eventually fails to trigger on pacakge update, due to exception in
    _projectMap.Values.SelectMany(x => x.GetAllContainers());
    
    This is fixed by updating the dictionary of testcontianers in ProjectInfo to be a concurrent dictionary.
    - delay adding the new project's testcontainers into the projectMap until we are finished building it.
    - new test that creates the exception when projectInfo used a regular dictionary
    
    * updating test comments
    
    * reducing test loops and adding more comments
    
    * minor text changes
    
    [skip ci]

---
+++ Python/Tests/TestAdapterTests/Mocks/MockPythonProject.cs
@@ -0,0 +1,67 @@
+﻿
+// Python Tools for Visual Studio
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.PythonTools.Interpreter;
+using Microsoft.PythonTools.Projects;
+
+namespace TestAdapterTests.Mocks {
+    public class MockPythonProject : PythonProject {
+        public MockPythonProject(string projectHome, string projectName) {
+            ProjectHome = projectHome;
+            ProjectName = projectName;
+        }
+
+        [Obsolete]
+        public override ProjectAnalyzer Analyzer => throw new NotImplementedException();
+
+        public override string ProjectHome { get; }
+
+        public override string ProjectName { get; }
+
+#pragma warning disable 67
+        public override event EventHandler ProjectAnalyzerChanged;
+        public override event EventHandler<PythonProjectPropertyChangedArgs> ProjectPropertyChanged;
+        public override event EventHandler ActiveInterpreterChanged;
+#pragma warning restore  67
+
+        public override Task<ProjectAnalyzer> GetAnalyzerAsync() {
+            throw new NotImplementedException();
+        }
+
+        public override IPythonInterpreterFactory GetInterpreterFactory() {
+            throw new NotImplementedException();
+        }
+
+        public override LaunchConfiguration GetLaunchConfigurationOrThrow() {
+            throw new NotImplementedException();
+        }
+
+        public override string GetProperty(string name) {
+            throw new NotImplementedException();
+        }
+
+        public override string GetUnevaluatedProperty(string name) {
+            throw new NotImplementedException();
+        }
+
+        public override void SetProperty(string name, string value) {
+            throw new NotImplementedException();
+        }
+    }
+}
