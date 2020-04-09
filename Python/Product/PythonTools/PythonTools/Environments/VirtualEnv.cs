19dc06b118dfd1543c5311fa4cd4216b7be632e9
Bill Schnurr <bschnurr@microsoft.com>
2020-04-09 16:10:02 -0700

    fix for python 3.8 from microsoft store failing to add new venv
    new versions of python >= 3.8 from the windows store have a different executable name (ie. python3.8.exe) in the registry InstallPath, compared to what is in the venv folder (python.exe) so search for both if interpExe is not found.
    
    Fix #6082

--- Python/Product/PythonTools/PythonTools/Environments/VirtualEnv.cs
+++ Python/Product/PythonTools/PythonTools/Environments/VirtualEnv.cs
@@ -405,8 +405,13 @@ namespace Microsoft.PythonTools.Environments {
             interpExe = Path.GetFileName(baseInterpreter.Configuration.InterpreterPath);
             winterpExe = Path.GetFileName(baseInterpreter.Configuration.GetWindowsInterpreterPath());
             var scripts = new[] { "Scripts", "bin" };
+
+            // new versions of python >= 3.8 from the windows store have a different executable name (ie. python3.8.exe) in the 
+            // registry InstallPath, compared to what is in the venv folder (python.exe) so search for both if interpExe is not found.
             interpExe = PathUtils.FindFile(prefixPath, interpExe, firstCheck: scripts);
+            interpExe = interpExe == null ? PathUtils.FindFile(prefixPath, "python.exe", firstCheck: scripts) : interpExe;
             winterpExe = PathUtils.FindFile(prefixPath, winterpExe, firstCheck: scripts);
+            winterpExe = winterpExe == null ? PathUtils.FindFile(prefixPath, "pythonw.exe", firstCheck: scripts) : winterpExe;
             pathVar = baseInterpreter.Configuration.PathEnvironmentVariable;
         }
 
