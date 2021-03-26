e899af4a7be8ab3451d4b96e5aecf5afad659f83
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-03-26 16:36:33 -0700

    Update comments

--- Python/Product/PythonTools/PythonTools/LanguageServerClient/StreamHacking/StreamIntercepter.cs
+++ Python/Product/PythonTools/PythonTools/LanguageServerClient/StreamHacking/StreamIntercepter.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Python Tools for Visual Studio
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.PythonTools.LanguageServerClient.StreamHacking {
+    /// <summary>
+    /// This class wraps a stream so that writes to the stream can be changed and reads to the stream can be followed.
+    /// </summary>
     class StreamIntercepter : Stream {
         private Stream baseStream;
         private Func<StreamData, StreamData> writeHandler;
