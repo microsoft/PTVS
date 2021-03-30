e24b5b81ff30367e34732dff2527f08710481695
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-03-30 10:29:44 -0700

    Try how Azure NBs does it

--- Build/UpdateNpmrc.ps1
+++ Build/UpdateNpmrc.ps1
@@ -2,16 +2,16 @@
 # Microsoft, doesn't need to be secret in pipeline
 # See here on how to generate a new token:
 # https://devdiv.visualstudio.com/DevDiv/_packaging?_a=connect&feed=Pylance%40Local
-$str = "registry=https://devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/ 
-                        
+$str = "registry=https://registry.npmjs.org/
+@pylance:registry=https://msdata.pkgs.visualstudio.com/_packaging/DSVM_Image/npm/registry/
 always-auth=true
 ; begin auth token
-//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:username=devdiv
-//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN
-//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:email=npm requires email to be set but doesn't use the value
-//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:username=devdiv
-//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN
-//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:email=npm requires email to be set but doesn't use the value
+//msdata.pkgs.visualstudio.com/_packaging/DSVM_Image/npm/registry/:username=msdata
+//msdata.pkgs.visualstudio.com/_packaging/DSVM_Image/npm/registry/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN
+//msdata.pkgs.visualstudio.com/_packaging/DSVM_Image/npm/registry/:email=email
+//msdata.pkgs.visualstudio.com/_packaging/DSVM_Image/npm/:username=msdata
+//msdata.pkgs.visualstudio.com/_packaging/DSVM_Image/npm/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN
+//msdata.pkgs.visualstudio.com/_packaging/DSVM_Image/npm/:email=email
 ; end auth token
 "
 
