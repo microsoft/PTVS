00934d1e8b16a582c25a970ef3575fd0f427643c
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-03-30 14:34:14 -0700

    Local version is this one

--- Build/UpdateNpmrc.ps1
+++ Build/UpdateNpmrc.ps1
@@ -2,10 +2,16 @@
 # Microsoft, doesn't need to be secret in pipeline
 # See here on how to generate a new token:
 # https://devdiv.visualstudio.com/DevDiv/_packaging?_a=connect&feed=Pylance%40Local
-$str = "_auth=$Env:MAPPED_AZURE_DEVOPS_TOKEN
+$str = "registry=https://devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/ 
 always-auth=true
-email=no-reply@example.com
-registry=http://devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry
+; begin auth token
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:username=devdiv
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:email=npm requires email to be set but doesn't use the value
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:username=devdiv
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:email=npm requires email to be set but doesn't use the value
+; end auth token
 "
 
 Write-Host "Writing .npmrc with token $Env:MAPPED_AZURE_DEVOPS_TOKEN from environment"
