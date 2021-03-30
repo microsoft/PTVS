d3e8060bfff569f39eba061b28cfe3b2d74ce9a3
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-03-30 09:58:21 -0700

    Try different way to authenticate

--- Build/UpdateNpmrc.ps1
+++ Build/UpdateNpmrc.ps1
@@ -1,13 +1,17 @@
 # DEVOPS_TOKEN is not secret so that it works on other branches. Since Devops pipeline is only accessible from 
 # Microsoft, doesn't need to be secret in pipeline
+$str = "
+registry=https://devdiv.pkgs.visualstudio.com/_packaging/Pylance/npm/registry/
+always-auth=true
+; begin auth token
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:username=devdiv
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:_password=[BASE64_ENCODED_PERSONAL_ACCESS_TOKEN]
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:email=npm requires email to be set but doesn't use the value
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:username=devdiv
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:_password=[BASE64_ENCODED_PERSONAL_ACCESS_TOKEN]
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:email=npm requires email to be set but doesn't use the value
+; end auth token
+"
+
 Write-Host "Writing .npmrc with token $Env:MAPPED_AZURE_DEVOPS_TOKEN from environment"
-Set-Content -Path ".npmrc" -Value "registry=https://devdiv.pkgs.visualstudio.com/_packaging/Pylance/npm/registry/"
-Add-Content -Path ".npmrc" -Value "always-auth=true"
-Add-Content -Path ".npmrc" -Value "; begin auth token"
-Add-Content -Path ".npmrc" -Value "//devdiv.pkgs.visualstudio.com/_packaging/Pylance/npm/registry/:username=devdiv"
-Add-Content -Path ".npmrc" -Value "//devdiv.pkgs.visualstudio.com/_packaging/Pylance/npm/registry/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN"
-Add-Content -Path ".npmrc" -Value "//devdiv.pkgs.visualstudio.com/_packaging/Pylance/npm/registry/:email=npm requires email to be set but doesn't use the value"
-Add-Content -Path ".npmrc" -Value "//devdiv.pkgs.visualstudio.com/_packaging/Pylance/npm/:username=devdiv"
-Add-Content -Path ".npmrc" -Value "//devdiv.pkgs.visualstudio.com/_packaging/Pylance/npm/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN"
-Add-Content -Path ".npmrc" -Value "//devdiv.pkgs.visualstudio.com/_packaging/Pylance/npm/:email=npm requires email to be set but doesn't use the value"
-Add-Content -Path ".npmrc" -Value "; end auth token"
+Set-Content -Path ".npmrc" -Value $str
