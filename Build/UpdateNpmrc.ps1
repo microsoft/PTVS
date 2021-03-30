4661bc40a26c1142c580f98deb25d935e94240b6
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-03-30 10:04:40 -0700

    Try without https

--- Build/UpdateNpmrc.ps1
+++ Build/UpdateNpmrc.ps1
@@ -1,14 +1,14 @@
 # DEVOPS_TOKEN is not secret so that it works on other branches. Since Devops pipeline is only accessible from 
 # Microsoft, doesn't need to be secret in pipeline
 $str = "
-registry=https://devdiv.pkgs.visualstudio.com/_packaging/Pylance/npm/registry/
+registry=http://devdiv.pkgs.visualstudio.com/_packaging/Pylance/npm/registry/
 always-auth=true
 ; begin auth token
 //devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:username=devdiv
-//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:_password=[BASE64_ENCODED_PERSONAL_ACCESS_TOKEN]
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN
 //devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:email=npm requires email to be set but doesn't use the value
 //devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:username=devdiv
-//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:_password=[BASE64_ENCODED_PERSONAL_ACCESS_TOKEN]
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN
 //devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:email=npm requires email to be set but doesn't use the value
 ; end auth token
 "
