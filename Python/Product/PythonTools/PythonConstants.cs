82a86accb57b9376216d2ca250723f6507c92ba3
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-04-30 17:43:13 -0700

    Attempt at reimplementing ctrl+enter

--- Python/Product/PythonTools/PythonConstants.cs
+++ Python/Product/PythonTools/PythonConstants.cs
@@ -45,7 +45,7 @@ namespace Microsoft.PythonTools {
         internal const string LibraryManagerServiceGuid = "88888859-2f95-416e-9e2b-cac4678e5af7";
         public const string ProjectFactoryGuid = "888888a0-9f3d-457c-b088-3a5042f75d52";
         internal const string WebProjectFactoryGuid = "1b580a1a-fdb3-4b32-83e1-6407eb2722e6";
-        //internal const string EditorFactoryGuid = "888888c4-36f9-4453-90aa-29fa4d2e5706";
+        internal const string EditorFactoryGuid = "888888c4-36f9-4453-90aa-29fa4d2e5706";
         internal const string ProjectNodeGuid = "8888881a-afb8-42b1-8398-e60d69ee864d";
         public const string GeneralPropertyPageGuid = "888888fd-3c4a-40da-aefb-5ac10f5e8b30";
         public const string DebugPropertyPageGuid = "9A46BC86-34CB-4597-83E5-498E3BDBA20A";
