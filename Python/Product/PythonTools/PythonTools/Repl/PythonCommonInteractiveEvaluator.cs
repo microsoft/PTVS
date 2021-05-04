6b93c05983f71fb255a31c9eec5e009888c2fd9d
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-05-04 11:19:31 -0700

    Skip settings change to prevent node crash

--- Python/Product/PythonTools/PythonTools/Repl/PythonCommonInteractiveEvaluator.cs
+++ Python/Product/PythonTools/PythonTools/Repl/PythonCommonInteractiveEvaluator.cs
@@ -154,7 +154,7 @@ namespace Microsoft.PythonTools.Repl {
                 await PythonLanguageClient.ReadyTask;
                 var client = _serviceProvider.GetPythonToolsService().LanguageClient;
                 if (client != null) {
-                    client.AddClientContext(new PythonLanguageClientContextRepl(evaluator), true);
+                    client.AddClientContext(new PythonLanguageClientContextRepl(evaluator)); // Don't fire a settings change. This can crash node for some reason
                     Document = new ReplDocument(_serviceProvider, _window, client);
                     await Document.InitializeAsync();
                 }
