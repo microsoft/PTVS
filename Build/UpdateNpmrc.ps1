7c9e259519701834d2f6995f3eec2f0658415ffc
Bill Schnurr <bschnurr@microsoft.com>
2021-06-15 13:06:40 -0700

    pipeline update fixing internal BUG 1327788 (#6572)

--- Build/UpdateNpmrc.ps1
+++ Build/UpdateNpmrc.ps1
@@ -5,10 +5,10 @@
 $str = "registry=https://devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/ 
 always-auth=true
 ; begin auth token
-//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:username=devdiv
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:username=$Env:MAPPED_AZURE_DEVOPS_USERNAME
 //devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN
 //devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/registry/:email=npm requires email to be set but doesn't use the value
-//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:username=devdiv
+//devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:username=$Env:MAPPED_AZURE_DEVOPS_USERNAME
 //devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:_password=$Env:MAPPED_AZURE_DEVOPS_TOKEN
 //devdiv.pkgs.visualstudio.com/_packaging/Pylance%40Local/npm/:email=npm requires email to be set but doesn't use the value
 ; end auth token
