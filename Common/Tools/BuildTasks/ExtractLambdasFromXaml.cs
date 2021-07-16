a208cc62ffc8ec8929082cef06a517fa28394d6f
Steven Cox <peezoslug@gmail.com>
2021-07-16 09:19:08 -0400

    4

--- Common/Tools/BuildTasks/ExtractLambdasFromXaml.cs
+++ Common/Tools/BuildTasks/ExtractLambdasFromXaml.cs
@@ -14,26 +14,16 @@
 // See the Apache Version 2.0 License for specific language governing
 // permissions and limitations under the License.
 
-using System;
-using System.CodeDom;
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Globalization;
-using System.IO;
-using System.Reflection;
-using System.Xaml;
-using System.Xml;
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 using Microsoft.VisualStudioTools.Wpf;
 
 #pragma warning disable CA1303 // Do not pass literals as localized parameters (this is a build utility that does not need localization)
 
-namespace Microsoft.VisualStudioTools.BuildTasks {
-    public class ExtractLambdasFromXaml : Task {
-        private struct LambdaInfo {
+namespace Microsoft.VisualStudioTools.BuildTasks
+{
+    public class ExtractLambdasFromXaml : Task
+    {
+        private struct LambdaInfo
+        {
             public string Code { get; set; }
             public int LineNumber { get; set; }
         }
@@ -79,75 +69,99 @@ namespace Microsoft.VisualStudioTools.BuildTasks {
             "System.Windows.Navigation"
         };
 
-        public ExtractLambdasFromXaml() {
+        public ExtractLambdasFromXaml()
+        {
             Language = "CSharp";
         }
 
-        public override bool Execute() {
-            try {
-                try {
+        public override bool Execute()
+        {
+            try
+            {
+                try
+                {
                     codeDomProvider = CodeDomProvider.CreateProvider(Language);
-                } catch (ConfigurationErrorsException) {
+                }
+                catch (ConfigurationErrorsException)
+                {
                     LogError(classNameLineNumber, 1, "CodeDom provider for language '" + Language + "' not found.");
                     return false;
                 }
 
                 typeAttributesConverter = codeDomProvider.GetConverter(typeof(TypeAttributes));
 
-                if (!ParseInput()) {
+                if (!ParseInput())
+                {
                     return false;
                 }
 
-                if (lambdas.Count == 0) {
+                if (lambdas.Count == 0)
+                {
                     OutputFileName = null;
                     return true;
                 }
 
-                if (className == null) {
+                if (className == null)
+                {
                     LogError(classNameLineNumber, 1501, "x:Class not found on root element.");
                     return false;
                 }
-                if (!className.Contains(".")) {
+                if (!className.Contains("."))
+                {
                     LogError(classNameLineNumber, 1502, "x:Class does not include namespace name.");
                     return false;
                 }
 
                 return GenerateOutput();
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 LogError(null, 0, 0, ex.Message);
                 throw;
             }
         }
 
-        private void LogError(string source, int line, int code, string text) {
+        private void LogError(string source, int line, int code, string text)
+        {
             string xalCode = string.Format(CultureInfo.InvariantCulture, "XAL{0:D4}", code);
 
-            if (source == null) {
+            if (source == null)
+            {
                 source = ToolName;
             }
 
-            if (BuildEngine != null) {
+            if (BuildEngine != null)
+            {
                 Log.LogError(null, xalCode, null, source, line, 0, 0, 0, text);
-            } else {
+            }
+            else
+            {
                 string pos = (line != 0) ? ("(" + line + ")") : " ";
                 Console.Error.WriteLine("{0}{1}: error {2}: {3}", source, pos, xalCode, text);
             }
         }
 
-        private void LogError(int code, string text) {
+        private void LogError(int code, string text)
+        {
             LogError(InputFileName.ItemSpec, 0, code, text);
         }
 
-        private void LogError(int line, int code, string text) {
+        private void LogError(int line, int code, string text)
+        {
             LogError(InputFileName.ItemSpec, line, code, text);
         }
 
-        private bool ParseInput() {
+        private bool ParseInput()
+        {
             XamlXmlReader reader = null;
-            try {
-                try {
+            try
+            {
+                try
+                {
                     reader = new XamlXmlReader(InputFileName.ItemSpec, new XamlXmlReaderSettings { ProvideLineInfo = true });
-                } catch (FileNotFoundException ex) {
+                }
+                catch (FileNotFoundException ex)
+                {
                     LogError(1001, ex.Message);
                     return false;
                 }
@@ -155,48 +169,66 @@ namespace Microsoft.VisualStudioTools.BuildTasks {
                 bool classNameExpected = false, classModifierExpected = false;
                 bool lambdaBodyExpected = false, importedNamespacesExpected = false;
                 int nestingLevel = 0, lambdaNestingLevel = -1;
-                while (reader.Read()) {
-                    switch (reader.NodeType) {
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
                         case XamlNodeType.GetObject:
                             ++nestingLevel;
                             break;
 
                         case XamlNodeType.StartObject:
                             ++nestingLevel;
-                            if (nestingLevel == 1) {
+                            if (nestingLevel == 1)
+                            {
                                 classNameLineNumber = reader.LineNumber;
                             }
-                            if ((reader.Type.Name == "Lambda" || reader.Type.Name == "LambdaExtension") && IsLambdaNamespace(reader.Type.PreferredXamlNamespace)) {
+                            if ((reader.Type.Name == "Lambda" || reader.Type.Name == "LambdaExtension") && IsLambdaNamespace(reader.Type.PreferredXamlNamespace))
+                            {
                                 lambdaNestingLevel = nestingLevel;
                             }
                             break;
 
                         case XamlNodeType.EndObject:
                             --nestingLevel;
-                            if (nestingLevel < lambdaNestingLevel) {
+                            if (nestingLevel < lambdaNestingLevel)
+                            {
                                 lambdaNestingLevel = -1;
                             }
                             break;
 
                         case XamlNodeType.StartMember:
-                            if (nestingLevel == 1) {
-                                if (reader.Member.PreferredXamlNamespace == XamlLanguage.Xaml2006Namespace) {
-                                    switch (reader.Member.Name) {
-                                        case "Class": {
+                            if (nestingLevel == 1)
+                            {
+                                if (reader.Member.PreferredXamlNamespace == XamlLanguage.Xaml2006Namespace)
+                                {
+                                    switch (reader.Member.Name)
+                                    {
+                                        case "Class":
+                                            {
                                                 classNameExpected = true;
-                                            } break;
+                                            }
+                                            break;
 
-                                        case "ClassModifier": {
+                                        case "ClassModifier":
+                                            {
                                                 classModifierExpected = true;
-                                            } break;
+                                            }
+                                            break;
                                     }
-                                } else if (reader.Member.DeclaringType != null && IsLambdaNamespace(reader.Member.DeclaringType.PreferredXamlNamespace)) {
-                                    if (reader.Member.Name == "ImportedNamespaces" && reader.Member.DeclaringType.Name == "LambdaProperties") {
+                                }
+                                else if (reader.Member.DeclaringType != null && IsLambdaNamespace(reader.Member.DeclaringType.PreferredXamlNamespace))
+                                {
+                                    if (reader.Member.Name == "ImportedNamespaces" && reader.Member.DeclaringType.Name == "LambdaProperties")
+                                    {
                                         importedNamespacesExpected = true;
                                     }
                                 }
-                            } else if (nestingLevel == lambdaNestingLevel) {
-                                if (reader.Member == XamlLanguage.UnknownContent || reader.Member == XamlLanguage.PositionalParameters || reader.Member.Name == "Lambda") {
+                            }
+                            else if (nestingLevel == lambdaNestingLevel)
+                            {
+                                if (reader.Member == XamlLanguage.UnknownContent || reader.Member == XamlLanguage.PositionalParameters || reader.Member.Name == "Lambda")
+                                {
                                     lambdaBodyExpected = true;
                                 }
                             }
@@ -207,35 +239,51 @@ namespace Microsoft.VisualStudioTools.BuildTasks {
                             break;
 
                         case XamlNodeType.Value:
-                            if (classNameExpected) {
+                            if (classNameExpected)
+                            {
                                 classNameExpected = false;
                                 className = (string)reader.Value;
                                 classNameLineNumber = reader.LineNumber;
-                            } else if (classModifierExpected) {
+                            }
+                            else if (classModifierExpected)
+                            {
                                 classModifierExpected = false;
                                 classModifier = (string)reader.Value;
                                 classModifierLineNumber = reader.LineNumber;
-                            } else if (importedNamespacesExpected) {
+                            }
+                            else if (importedNamespacesExpected)
+                            {
                                 importedNamespaces.Clear();
                                 importedNamespaces.AddRange(((string)reader.Value).Split(" \f\n\r\t".ToCharArray(), StringSplitOptions.RemoveEmptyEntries));
                                 importedNamespacesLineNumber = reader.LineNumber;
-                            } else if (lambdaBodyExpected) {
+                            }
+                            else if (lambdaBodyExpected)
+                            {
                                 lambdas.Add(new LambdaInfo { Code = (string)reader.Value, LineNumber = reader.LineNumber });
                             }
                             break;
                     }
                 }
-            } catch (IOException ex) {
+            }
+            catch (IOException ex)
+            {
                 LogError(1002, ex.Message);
                 return false;
-            } catch (XmlException ex) {
+            }
+            catch (XmlException ex)
+            {
                 LogError(1003, ex.Message);
                 return false;
-            } catch (XamlException ex) {
+            }
+            catch (XamlException ex)
+            {
                 LogError(1004, ex.Message);
                 return false;
-            } finally {
-                if (reader != null) {
+            }
+            finally
+            {
+                if (reader != null)
+                {
                     reader.Close();
                 }
             }
@@ -243,31 +291,43 @@ namespace Microsoft.VisualStudioTools.BuildTasks {
             return true;
         }
 
-        private bool GenerateOutput() {
+        private bool GenerateOutput()
+        {
             string shortClassName, classNamespace;
             int dot = className.LastIndexOf('.');
-            if (dot >= 0) {
+            if (dot >= 0)
+            {
                 classNamespace = className.Substring(0, dot);
                 shortClassName = className.Substring(dot + 1);
-            } else {
+            }
+            else
+            {
                 classNamespace = null;
                 shortClassName = className;
             }
 
             bool isPrivate = false;
-            if (classModifier != null) {
+            if (classModifier != null)
+            {
                 string publicModifier = null, privateModifier = null;
-                if (typeAttributesConverter != null && typeAttributesConverter.CanConvertTo(typeof(string))) {
-                    try {
+                if (typeAttributesConverter != null && typeAttributesConverter.CanConvertTo(typeof(string)))
+                {
+                    try
+                    {
                         publicModifier = typeAttributesConverter.ConvertTo(TypeAttributes.Public, typeof(string)) as string;
                         privateModifier = typeAttributesConverter.ConvertTo(TypeAttributes.NotPublic, typeof(string)) as string;
-                    } catch (NotSupportedException) {
+                    }
+                    catch (NotSupportedException)
+                    {
                     }
                 }
 
-                if (string.Equals(classModifier, privateModifier, StringComparison.OrdinalIgnoreCase)) {
+                if (string.Equals(classModifier, privateModifier, StringComparison.OrdinalIgnoreCase))
+                {
                     isPrivate = true;
-                } else if (!string.Equals(classModifier, publicModifier, StringComparison.OrdinalIgnoreCase)) {
+                }
+                else if (!string.Equals(classModifier, publicModifier, StringComparison.OrdinalIgnoreCase))
+                {
                     LogError(classModifierLineNumber, 1503, "Language '" + Language + "' does not support x:ClassModifier '" + classModifier + "'.");
                     return false;
                 }
@@ -277,29 +337,34 @@ namespace Microsoft.VisualStudioTools.BuildTasks {
 
             var ns = new CodeNamespace(classNamespace);
             unit.Namespaces.Add(ns);
-            foreach (string importName in importedNamespaces) {
+            foreach (string importName in importedNamespaces)
+            {
                 var import = new CodeNamespaceImport(importName);
-                if (importedNamespacesLineNumber != 0) {
+                if (importedNamespacesLineNumber != 0)
+                {
                     import.LinePragma = new CodeLinePragma(InputFileName.ItemSpec, importedNamespacesLineNumber);
                 }
                 ns.Imports.Add(import);
             }
-            var type = new CodeTypeDeclaration {
+            var type = new CodeTypeDeclaration
+            {
                 Name = shortClassName,
                 IsPartial = true,
                 BaseTypes = { typeof(ILambdaConverterProvider) }
             };
             ns.Types.Add(type);
-            if (isPrivate) {
+            if (isPrivate)
+            {
                 type.TypeAttributes &= ~TypeAttributes.Public;
             }
 
-            var method = new CodeMemberMethod {
+            var method = new CodeMemberMethod
+            {
                 Name = "GetConverterForLambda",
                 PrivateImplementationType = new CodeTypeReference(typeof(ILambdaConverterProvider)),
                 ReturnType = new CodeTypeReference(typeof(LambdaConverter)),
                 Parameters =
-                {   
+                {
                     new CodeParameterDeclarationExpression
                     {
                         Name = "lambda__",
@@ -310,7 +375,7 @@ namespace Microsoft.VisualStudioTools.BuildTasks {
                 {
                     new CodeAttributeDeclaration(new CodeTypeReference(typeof(GeneratedCodeAttribute)))
                     {
-                        Arguments = 
+                        Arguments =
                         {
                             new CodeAttributeArgument(new CodePrimitiveExpression(ToolName)),
                             new CodeAttributeArgument(new CodePrimitiveExpression(typeof(ExtractLambdasFromXaml).Assembly.GetName().Version.ToString()))
@@ -320,9 +385,12 @@ namespace Microsoft.VisualStudioTools.BuildTasks {
             };
             type.Members.Add(method);
 
-            foreach (var lambda in lambdas) {
-                var cond = new CodeConditionStatement {
-                    Condition = new CodeBinaryOperatorExpression {
+            foreach (var lambda in lambdas)
+            {
+                var cond = new CodeConditionStatement
+                {
+                    Condition = new CodeBinaryOperatorExpression
+                    {
                         Operator = CodeBinaryOperatorType.ValueEquality,
                         Left = new CodeArgumentReferenceExpression("lambda__"),
                         Right = new CodePrimitiveExpression(lambda.Code)
@@ -356,8 +424,10 @@ namespace Microsoft.VisualStudioTools.BuildTasks {
             }
 
             method.Statements.Add(
-                new CodeThrowExceptionStatement {
-                    ToThrow = new CodeObjectCreateExpression {
+                new CodeThrowExceptionStatement
+                {
+                    ToThrow = new CodeObjectCreateExpression
+                    {
                         CreateType = new CodeTypeReference(typeof(ArgumentOutOfRangeException)),
                         Parameters =
                         {
@@ -366,12 +436,16 @@ namespace Microsoft.VisualStudioTools.BuildTasks {
                     }
                 });
 
-            try {
-                using (var writer = File.CreateText(OutputFileName.ItemSpec)) {
+            try
+            {
+                using (var writer = File.CreateText(OutputFileName.ItemSpec))
+                {
                     var options = new CodeGeneratorOptions();
                     codeDomProvider.GenerateCodeFromCompileUnit(unit, writer, options);
                 }
-            } catch (IOException ex) {
+            }
+            catch (IOException ex)
+            {
                 LogError(2002, ex.Message);
                 return false;
             }
@@ -382,7 +456,8 @@ namespace Microsoft.VisualStudioTools.BuildTasks {
         private static readonly string LambdaConverterClrNamespace = "clr-namespace:" + typeof(LambdaConverter).Namespace;
         private static readonly string LambdaConverterClrNamespaceWithAssembly = LambdaConverterClrNamespace + ";assembly=";
 
-        private static bool IsLambdaNamespace(string ns) {
+        private static bool IsLambdaNamespace(string ns)
+        {
             return ns != null && (ns == LambdaConverterClrNamespace ||
                 ns.StartsWith(LambdaConverterClrNamespaceWithAssembly, false, CultureInfo.InvariantCulture));
         }
