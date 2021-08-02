1d6bb8b049d0ceb487289afba56b0c9e7b24520f
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-08-02 16:09:55 -0700

    Two accessibility fixes (#6634)

--- Python/Product/EnvironmentsList/Resources.Designer.cs
+++ Python/Product/EnvironmentsList/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.PythonTools.EnvironmentsList {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -621,7 +621,7 @@ namespace Microsoft.PythonTools.EnvironmentsList {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Enable IPython interactive mode.
+        ///   Looks up a localized string similar to Use IPython interactive mode.
         /// </summary>
         public static string EnvironmentPathsExtensionEnableIPythonAutomationName {
             get {
