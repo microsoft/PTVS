47c60573b3016de866cdbd6ca4dc3c063e8346cb
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-08-18 14:21:50 -0700

    Respond to different screen widths by enforcing portrait mode and allowing all controls to scroll

---
+++ Python/Product/PythonTools/PythonTools/Environments/ScreenAdjustingConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Microsoft.PythonTools.Wpf;
+
+namespace Microsoft.PythonTools.Environments {
+    /// <summary>
+    /// Converts parameters into styles based on landscape or portrait mode
+    /// </summary>
+    public sealed class ScreenAdjustingConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            // parameter should be wanted width
+            var width = parameter != null ? System.Convert.ToDouble(parameter) : -1;
+
+            // Parameter should be the width we want
+            if (width > 0) {
+                // Ideally we'd use the actual width of the control, but at this
+                // point the control is not rendered. So instead scale based on screen width
+                // only care for situations where the width is less than 1200 (above 1200 we don't have a problem)
+                if (SystemParameters.PrimaryScreenWidth < 1200) {
+                    // How much less than 1200?
+                    var percentOfBigger = SystemParameters.PrimaryScreenWidth / 1200;
+                    return width * percentOfBigger;
+                }
+
+                return width;
+            }
+            return value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
