75ec9d8f65b91157ff3790167f1c1acee508754c
Steven Cox <peezoslug@gmail.com>
2021-11-12 15:06:39 -0500

    pylance

--- Build/16.0/packages_config_updater.py
+++ Build/16.0/packages_config_updater.py
@@ -14,7 +14,7 @@
 # See the Apache Version 2.0 License for specific language governing
 # permissions and limitations under the License.
 
-from lxml import html
+from xml import html
 import requests
 
 #How to use this script
