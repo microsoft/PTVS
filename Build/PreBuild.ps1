85c1ac2be41a1ecc3ca4033493de3bd15d9a3892
Adam Yoblick <Adam.Yoblick@microsoft.com>
2022-01-07 15:22:52 -0600

    install passed in pylance version

--- Build/PreBuild.ps1
+++ Build/PreBuild.ps1
@@ -1,4 +1,4 @@
-param ($pylanceTgz, $vstarget, $source, $outdir)
+param ($vstarget, $outdir, $pylanceVersion)
 
 $ErrorActionPreference = "Stop"
 
@@ -23,6 +23,10 @@ if (-not $vstarget) {
     $vstarget = "$vstarget.0"
 }
 
+if (-not $pylanceVersion) {
+    $pylanceVersion = "latest"
+}
+
 $buildroot = $MyInvocation.MyCommand.Definition | Split-Path -Parent | Split-Path -Parent
 
 if (-not $outdir) {
@@ -44,17 +48,25 @@ try {
     # See https://microsoft.sharepoint.com/teams/python/_layouts/15/Doc.aspx?sourcedoc=%7B30d33826-9f98-4d3e-890e-b7d198bbbcbe%7D&action=edit&wd=target(Python%20VS%2FDev%20Docs.one%7Cd7206ce2-cf40-437b-8ce9-1e55f4bc2f44%2FPylance%20in%20VS%7C6000d391-4e62-4a4d-89d2-7f7c1f005639%2F)&share=IgEmONMwmJ8-TYkOt9GYu7y-AeCM6R8r8Myty0Lj8CeOs4E
     # Note that this will modify your package-lock.json file if the version was updated. This file should be committed into source control.
     "Installing Pylance"
+
+    # overwrite the pylance version in the package.json with the specified version
+    $packageJsonFile = Join-Path $buildroot "package.json"
+    $packageJson = Get-Content $packageJsonFile -Raw | ConvertFrom-Json
+    # only overwrite if the values are different
+    if ($packageJson.devDependencies.'@pylance/pylance' -ne $pylanceVersion) {
+        $packageJson.devDependencies.'@pylance/pylance' = $pylanceVersion
+        # ConvertTo-Json has a default depth of 2, so make it bigger to avoid strange errors
+        $packageJson | ConvertTo-Json -depth 8 | Set-Content $packageJsonFile
+    }
+
     npm install --save
-    # add the package lock file changes into git
-    $packageLockPath = Join-Path $buildroot "package-lock.json"
-    git add $packageLockPath
-    # print the installed version
-    $output = & npm ls @pylance/pylance
-    $pylanceVersion = $output[1] -split "@" | Select-Object -Last 1
-    "Installed Pylance $pylanceVersion"
+    # print out the installed version
+    $npmLsOutput = & npm ls @pylance/pylance
+    $installedPylanceVersion = $npmLsOutput[1] -split "@" | Select-Object -Last 1
+    "Installed Pylance $installedPylanceVersion"
     # add azdo build tag
     # commenting this out for now since azdo is throwing errors for an unknown reason
-    #Write-Host "##vso[build.addbuildtag]Pylance-$pylanceVersion"
+    #Write-Host "##vso[build.addbuildtag]Pylance-$installedPylanceVersion"
 
     "Restoring Packages"
     $arglist = "restore", "$vstarget\packages.config", "-OutputDirectory", "`"$outdir`"", "-Config", "nuget.config", "-NonInteractive"
