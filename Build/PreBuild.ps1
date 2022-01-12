c47b355324ea76343c9c4879b6f361fa396d2300
Adam Yoblick <Adam.Yoblick@microsoft.com>
2022-01-12 16:58:50 -0600

    Auto-update pylance and debugpy dependencies when running PreBuild.ps1 (#6848)

--- Build/PreBuild.ps1
+++ Build/PreBuild.ps1
@@ -1,8 +1,6 @@
-param ($pylanceTgz, $vstarget, $source, $outdir)
+param ($vstarget, $outdir, $pylanceVersion, $debugpyVersion)
 
 $ErrorActionPreference = "Stop"
-
-"Restoring Packages"
 
 # These packages require a versionless symlink pointing to the versioned install.
 $need_symlink = @(
@@ -25,6 +23,14 @@ if (-not $vstarget) {
     $vstarget = "$vstarget.0"
 }
 
+if (-not $pylanceVersion) {
+    $pylanceVersion = "latest"
+}
+
+if (-not $debugpyVersion) {
+    $debugpyVersion = "latest"
+}
+
 $buildroot = $MyInvocation.MyCommand.Definition | Split-Path -Parent | Split-Path -Parent
 
 if (-not $outdir) {
@@ -40,6 +46,37 @@ $outdir = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPat
 
 Push-Location "$buildroot\Build"
 try {
+
+    # Install pylance version specified in package.json
+    # If this doesn't work, you probably need to set up your .npmrc file or you need permissions to the feed.
+    # See https://microsoft.sharepoint.com/teams/python/_layouts/15/Doc.aspx?sourcedoc=%7B30d33826-9f98-4d3e-890e-b7d198bbbcbe%7D&action=edit&wd=target(Python%20VS%2FDev%20Docs.one%7Cd7206ce2-cf40-437b-8ce9-1e55f4bc2f44%2FPylance%20in%20VS%7C6000d391-4e62-4a4d-89d2-7f7c1f005639%2F)&share=IgEmONMwmJ8-TYkOt9GYu7y-AeCM6R8r8Myty0Lj8CeOs4E
+    # Note that this will modify your package-lock.json file if the version was updated. This file should be committed into source control.
+    "Installing Pylance"
+
+    # overwrite the pylance version in the package.json with the specified version
+    $packageJsonFile = Join-Path $buildroot "package.json"
+    $packageJson = Get-Content $packageJsonFile -Raw | ConvertFrom-Json
+    # only overwrite if the values are different
+    if ($packageJson.devDependencies.'@pylance/pylance' -ne $pylanceVersion) {
+        $packageJson.devDependencies.'@pylance/pylance' = $pylanceVersion
+        # ConvertTo-Json has a default depth of 2, so make it bigger to avoid strange errors
+        $packageJson | ConvertTo-Json -depth 8 | Set-Content $packageJsonFile
+    }
+
+    # install pylance and update the package-lock.json file
+    npm install --save
+
+    # print out the installed version
+    $npmLsOutput = & npm ls @pylance/pylance
+    $installedPylanceVersion = $npmLsOutput[1] -split "@" | Select-Object -Last 1
+    "Installed Pylance $installedPylanceVersion"
+    
+    # add azdo build tag
+    # commenting this out for now since azdo is throwing errors for an unknown reason
+    #Write-Host "##vso[build.addbuildtag]Pylance-$installedPylanceVersion"
+
+    "-----"
+    "Restoring Packages"
     $arglist = "restore", "$vstarget\packages.config", "-OutputDirectory", "`"$outdir`"", "-Config", "nuget.config", "-NonInteractive"
     $nuget = Get-Command nuget.exe -EA 0
     if (-not $nuget) {
@@ -48,9 +85,9 @@ try {
     Start-Process -Wait -NoNewWindow $nuget.Source -ErrorAction Stop -ArgumentList $arglist
 
     $versions = @{}
-    ([xml](Get-Content "$vstarget\packages.config")).packages.package | ForEach-Object{ $versions[$_.id] = $_.version }
+    ([xml](Get-Content "$vstarget\packages.config")).packages.package | ForEach-Object { $versions[$_.id] = $_.version }
 
-    $need_symlink | Where-Object{ $versions[$_] } | ForEach-Object{
+    $need_symlink | Where-Object { $versions[$_] } | ForEach-Object {
         $existing = Get-Item "$outdir\$_" -EA 0
         if ($existing) {
             if ($existing.LinkType) {
@@ -64,12 +101,39 @@ try {
         New-Item -ItemType Junction "$outdir\$_" -Value "$outdir\$_.$($versions[$_])"
     } | Out-Null
 
-    $debugpyver = Get-Content "$buildroot\Build\debugpy-version.txt" -Raw 
-    Write-Host "Downloading debugpy version $debugpyver"
-    $debugpyarglist = "install_debugpy.py", $debugpyver, "`"$outdir`""
-    Start-Process -Wait -NoNewWindow "$outdir\python\tools\python.exe" -ErrorAction Stop -ArgumentList $debugpyarglist
+    # debugpy install must come after package restore because it uses python which is symlinked as part of the previous step
 
-    Write-Host "Updating Microsoft.Python.*.dll pdbs to be windows format"
+    "-----"
+    "Installing Debugpy"
+    # pip install python packaging utilities
+    # SilentlyContinue on error since pip warnings will cause the build to fail, and installing debugpy will fail later if this step fails anyway
+    $pipArgList = "-m", "pip", "--disable-pip-version-check", "install", "packaging" 
+    Start-Process -Wait -NoNewWindow "$outdir\python\tools\python.exe" -ErrorAction SilentlyContinue -ArgumentList $pipArgList
+
+    # install debugpy
+    $debugpyArglist = "install_debugpy.py", $debugpyVersion, "`"$outdir`""
+    Start-Process -Wait -NoNewWindow "$outdir\python\tools\python.exe" -ErrorAction Stop -ArgumentList $debugpyArglist
+
+    # print out the installed version
+    $installedDebugpyVersion = ""
+    $versionPyFile = Join-Path $outdir "debugpy\_version.py"
+    foreach ($line in Get-Content $versionPyFile) {
+        if ($line.Trim().StartsWith("`"version`"")) {
+            $installedDebugpyVersion = $line.split(":")[1].Trim(" `"") # trim spaces and double quotes
+            break
+        }
+    }
+    "Installed Debugpy $installedDebugpyVersion"
+
+    # write debugpy version out to $buildroot\build\debugpy-version.txt, since that file is used by Debugger.csproj and various other classes
+    Set-Content -NoNewline -Force -Path "$buildroot\build\debugpy-version.txt" -Value $installedDebugpyVersion
+
+    # add azdo build tag
+    # commenting this out for now since azdo is throwing errors for an unknown reason
+    #Write-Host "##vso[build.addbuildtag]Debugpy-$installedDebugpyVersion"
+
+    "-----"
+    "Updating Microsoft.Python.*.dll pdbs to be windows format"
     Get-ChildItem "$outdir\Microsoft.Python.Parsing\lib\netstandard2.0" -Filter "*.pdb" | ForEach-Object {
         # Skip if there's already a pdb2 file
         # Convert each pdb $_.FullName
