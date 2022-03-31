a526574724a16169222a97ef85ade095bb5c82fc
Steven Cox <peezoslug@gmail.com>
2022-03-31 17:53:24 -0400

    31

--- App1/Resources/Resource.designer.cs
+++ App1/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace App1
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.99.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.0.155")]
 	public partial class Resource
 	{
 		
