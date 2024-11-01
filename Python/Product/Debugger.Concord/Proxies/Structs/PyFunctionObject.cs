b5cf45614f040ea9d40e30dc7aa98d479e992f99
Rich Chiodo false <rchiodo@microsoft.com>
2024-11-01 14:20:04 -0700

    Fix 3.10 thread state

---
+++ Python/Product/Debugger.Concord/Proxies/Structs/PyFunctionObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.PythonTools.Common.Parsing;
+using Microsoft.VisualStudio.Debugger;
+
+namespace Microsoft.PythonTools.Debugger.Concord.Proxies.Structs {
+    [StructProxy(StructName = "PyFunctionObject", MinVersion = PythonLanguageVersion.V311)]
+    internal class PyFunctionObject : PyObject {
+        internal class Fields {
+            public StructField<PointerProxy<PyObject>> func_globals;
+            public StructField<PointerProxy<PyObject>> func_builtins;
+            public StructField<PointerProxy<PyObject>> func_name;
+            public StructField<PointerProxy<PyObject>> func_qualname;
+            public StructField<PointerProxy<PyObject>> func_code;
+            public StructField<PointerProxy<PyObject>> func_defaults;
+            public StructField<PointerProxy<PyObject>> func_kwdefaults;
+            public StructField<PointerProxy<PyObject>> func_closure;
+            public StructField<PointerProxy<PyObject>> func_doc;
+            public StructField<PointerProxy<PyObject>> func_dict;
+            public StructField<PointerProxy<PyObject>> func_weakreflist;
+            public StructField<PointerProxy<PyObject>> func_module;
+            public StructField<PointerProxy<PyObject>> func_annotations;
+        }
+
+        private readonly Fields _fields;
+
+        public PyFunctionObject(DkmProcess process, ulong address)
+            : base(process, address) {
+            InitializeStruct(this, out _fields);
+            CheckPyType<PyFunctionObject>();
+        }
+    }
+}
---
+++ Python/Product/Debugger.Concord/Proxies/Structs/PyInterpreterFrame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.PythonTools.Common.Parsing;
+using Microsoft.VisualStudio.Debugger;
+
+namespace Microsoft.PythonTools.Debugger.Concord.Proxies.Structs {
+    // This was added in commit https://github.com/python/cpython/commit/ae0a2b756255629140efcbe57fc2e714f0267aa3
+    [StructProxy(StructName = "_PyInterpreterFrame", MinVersion = PythonLanguageVersion.V311)]
+    internal class PyInterpreterFrame : StructProxy {
+        internal class Fields {
+            public StructField<PointerProxy<PyFunctionObject>> f_func;
+            public StructField<PointerProxy<PyObject>> f_globals;
+            public StructField<PointerProxy<PyObject>> f_builtins;
+            public StructField<PointerProxy<PyObject>> f_locals;
+            public StructField<PointerProxy<PyCodeObject>> f_code;
+            public StructField<PointerProxy<PyFrameObject>> frame_obj;
+            public StructField<PointerProxy<PyInterpreterFrame>> previous;
+            public StructField<PointerProxy<PyObject>> prev_instr; // Not sure PyObject is the right type here
+            public StructField<Int32Proxy> stacktop;
+            public StructField<BoolProxy> is_entry;
+            public StructField<CharProxy> owner;
+            public StructField<PointerProxy<ArrayProxy<PyObject>>> localsplus;
+        }
+
+        private readonly Fields _fields;
+
+        public PyInterpreterFrame(DkmProcess process, ulong address)
+            : base(process, address) {
+            InitializeStruct(this, out _fields);
+        }
+    }
+}
