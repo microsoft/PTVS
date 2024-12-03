4578a57f77afb80798a9498c7eb71e29a72376f3
Rich Chiodo false <rchiodo@microsoft.com>
2024-12-03 09:40:10 -0800

    Verify pkgdef is copied

--- Build/setup_glass.py
+++ Build/setup_glass.py
@@ -161,6 +161,14 @@ def copy_ptvs_output(debug_output: bool = False):
         shutil.copy(file, glass_debugger_dir)
         shutil.copy(file, glass_remote_debugger_dir)
 
+    # Verify some of the files were copied.
+    if not os.path.exists(os.path.join(glass_debugger_dir, "Microsoft.PythonTools.Core.pkgdef")) or not os.path.exists(os.path.join(glass_remote_debugger_dir, "Microsoft.PythonTools.Core.pkgdef")):
+        print(f"Error: Microsoft.PythonTools.Core.pkgdef not found in {glass_debugger_dir} or {glass_remote_debugger_dir}.")
+        print(f"Contents of {glass_debugger_dir}:")
+        for f in os.listdir(glass_debugger_dir):
+            print(f)
+        exit(1)
+
     # Whenever we copy new bits, we have to regenerate the Python.GlassTestGroup file
     generate_python_version_props()
 
