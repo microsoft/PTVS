722f41aebaa8a07c0a9306b34a90a7e36028040e
bschnurr <bschnurr@microsoft.com>
2024-12-06 13:35:59 -0800

    change IsSupathOf to handle null or empty

--- Python/Product/PythonTools/PythonTools/LanguageServerClient/PythonLanguageClient.cs
+++ Python/Product/PythonTools/PythonTools/LanguageServerClient/PythonLanguageClient.cs
@@ -524,7 +524,7 @@ namespace Microsoft.PythonTools.LanguageServerClient {
         private void UpdateInterpreterExcludes() {
             this._clientContexts.ForEach(context => {
 
-                if (context.RootPath != null && PathUtils.IsSubpathOf(context.RootPath, context.InterpreterConfiguration.InterpreterPath)) {
+                if (PathUtils.IsSubpathOf(context.RootPath, context.InterpreterConfiguration.InterpreterPath)) {
                     var pattern = CommonUtils.GetRelativeFilePath(context.RootPath, context.InterpreterConfiguration.GetPrefixPath()).TrimEnd('\\') + "/**/*";
                     var watcher = new FileSystemWatcher() { GlobPattern = pattern };
                     this._fileListener.AddExclude(watcher);
